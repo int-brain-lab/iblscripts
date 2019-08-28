01e98b548331c6d994a541850039fd00ef02ec88
Olivier Winter <oliche@gmail.com>
2019-08-28 08:10:03 +0100

    Tests ephys TTL check

---
+++ tests/test_ephys_ttl_check.py
@@ -0,0 +1,23 @@
+"""
+The ttl_check pipeline is to validate a setup before doing recordings,
+on the ephysChoiceWorld task, doing a dummy without animal and check that all syncs are there
+"""
+import unittest
+from pathlib import Path
+
+from ibllib.io.extractors import ephys_fpga
+
+
+class TestEphysCheckList(unittest.TestCase):
+    def setUp(self):
+        self.init_folder = Path('/mnt/s0/Data/IntegrationTests/ephys/ttl_check')
+        if not self.init_folder.exists():
+            return
+
+    def test_checklist_mock_3B(self):
+        ses_path = self.init_folder / 'ttl_3B'
+        self.assertTrue(ephys_fpga.validate_ttl_test(ses_path))
+
+    def test_checklist_mock_3A(self):
+        ses_path = self.init_folder / 'ttl_3A'
+        self.assertTrue(ephys_fpga.validate_ttl_test(ses_path))
