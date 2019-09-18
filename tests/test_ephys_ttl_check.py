f362f064b74ddc25807879337be45a9355575ccf
olivier <olivier.winter@hotmail.fr>
2019-09-18 17:21:22 +0100

    test_ephys_ttl 3A dual probes

--- tests/test_ephys_ttl_check.py
+++ tests/test_ephys_ttl_check.py
@@ -17,6 +17,8 @@ class TestEphysCheckList(unittest.TestCase):
         self.init_folder = Path('/mnt/s0/Data/IntegrationTests/ephys/ttl_check')
         if not self.init_folder.exists():
             return
+        for sf in self.init_folder.joinpath('ttl_3B_single').rglob('_spikeglx_sync.*.npy'):
+            sf.unlink()
 
     def test_checklist_mock_3B_single(self):
         ses_path = self.init_folder / 'ttl_3B_single'
@@ -26,10 +28,20 @@ class TestEphysCheckList(unittest.TestCase):
         ses_path = self.init_folder / 'ttl_3A_single'
         self.assertTrue(ibllib.ephys.ephysqc.validate_ttl_test(ses_path))
 
+    def test_checklist_mock_3A_dual(self):
+        ses_path = self.init_folder / 'ttl_3A_dual'
+        self.assertTrue(ibllib.ephys.ephysqc.validate_ttl_test(ses_path))
+
+    def test_checklist_from_arbitrary_folder(self):
+        ses_path = self.init_folder / 'ttl_3A_dual'
+        self.assertTrue(ibllib.ephys.ephysqc.validate_ttl_test(ses_path))
+        ibllib.ephys.ephysqc.validate_ttl_test(ses_path / 'raw_ephys_data')
+
     def test_checklist_mock_3B_dual(self):
         ses_path = self.init_folder / 'ttl_3B_dual'
         self.assertTrue(ibllib.ephys.ephysqc.validate_ttl_test(ses_path))
 
-    def tearDown(self) -> None:
-        for sf in self.init_folder.rglob('_spikeglx_sync.*.npy'):
-            sf.unlink()
+    def test_checklist_empty_folder(self):
+        ses_path = self.init_folder / 'empty'
+        with self.assertRaises(FileNotFoundError):
+            ibllib.ephys.ephysqc.validate_ttl_test(ses_path)
