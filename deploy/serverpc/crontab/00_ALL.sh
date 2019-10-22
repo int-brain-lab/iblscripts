0070dd5b95ceecc859ea1b4416ff0f1a934363b3
olivier <olivier.winter@hotmail.fr>
2019-10-22 16:01:59 +0100

    Audio training part of the nightly run

--- deploy/serverpc/crontab/00_ALL.sh
+++ deploy/serverpc/crontab/00_ALL.sh
@@ -6,4 +6,5 @@ cd ~/Documents/PYTHON/iblscripts/deploy/serverpc/crontab
 
 ./01_extract_training.sh; ./02_register.sh
 ./20_extract_ephys.sh; ./02_register.sh
-./03_compress_videos.sh;  ./02_register.sh
+./03_compress_videos.sh; ./02_register.sh
+./04_audio_training.sh; ./02_register.sh
