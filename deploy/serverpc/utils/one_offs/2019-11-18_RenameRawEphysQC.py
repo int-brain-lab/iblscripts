b46f8eb1f8bc74f99cd396751748be066ab852be
olivier <olivier.winter@hotmail.fr>
2019-11-28 10:54:26 +0000

    ibllib version 1.3.8

--- deploy/serverpc/utils/one_offs/2019-11-18_RenameRawEphysQC.py
+++ deploy/serverpc/utils/one_offs/2019-11-18_RenameRawEphysQC.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 DRY = False
 root_path = Path("/datadisk/Data/IntegrationTests/ephys/choice_world_init")
-
+root_path = Path("/mnt/s1/Data/Subjects/ZM_1735/2019-08-01/001/raw_ephys_data")
 RENAMES = [('_spikeglx_ephysQcFreqAP.freq*.npy', '_iblqc_ephysSpectralDensityAP.freqs.npy'),
-           ('_spikeglx_ephysQcFreqAP.power*.npy', '_iblqc_ephysSpectralDensityAP.amps.npy'),
+           ('_spikeglx_ephysQcFreqAP.power*.npy', '_iblqc_ephysSpectralDensityAP.power.npy'),
            ('_spikeglx_ephysQcFreqLF.freq*.npy', '_iblqc_ephysSpectralDensityLF.freqs.npy'),
-           ('_spikeglx_ephysQcFreqLF.power*.npy', '_iblqc_ephysSpectralDensityLF.amps.npy'),
-           ('_spikeglx_ephysQcTimeAP.rms*.npy', '_iblqc_ephysTimeRmsAP.amps.npy'),
+           ('_spikeglx_ephysQcFreqLF.power*.npy', '_iblqc_ephysSpectralDensityLF.power.npy'),
+           ('_spikeglx_ephysQcTimeAP.rms*.npy', '_iblqc_ephysTimeRmsAP.rms.npy'),
            ('_spikeglx_ephysQcTimeAP.times*.npy', '_iblqc_ephysTimeRmsAP.timestamps.npy'),
-           ('_spikeglx_ephysQcTimeLF.rms*.npy', '_iblqc_ephysTimeRmsLF.amps.npy'),
+           ('_spikeglx_ephysQcTimeLF.rms*.npy', '_iblqc_ephysTimeRmsLF.rms.npy'),
            ('_spikeglx_ephysQcTimeLF.times*.npy', '_iblqc_ephysTimeRmsLF.timestamps.npy')]
 
 for rename in RENAMES:
