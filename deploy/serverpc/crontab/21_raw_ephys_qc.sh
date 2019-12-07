83cc3e341fba757c33b321a11ccb5e3c634cdf74
olivier <olivier.winter@hotmail.fr>
2019-12-07 10:07:13 +0000

    Add automatic ephys qc / compression

--- deploy/serverpc/crontab/21_raw_ephys_qc.sh
+++ deploy/serverpc/crontab/21_raw_ephys_qc.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source ~/Documents/PYTHON/envs/iblenv/bin/activate
 cd ~/Documents/PYTHON/iblscripts/deploy/serverpc/crontab
-python ephys.py raw_ephys_qc /mnt/s0/Data/Subjects/ --dry=False --count=5
+python ephys.py raw_ephys_qc /mnt/s0/Data/Subjects/ --dry=False --count=3
