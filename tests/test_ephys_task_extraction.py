0c1d9def9eaf532de7d1b4cd13d61eef808b22e7
olivier <olivier.winter@hotmail.fr>
2019-12-17 07:42:01 +0000

    bugfix: adjust bpod trial start length for Angelaki hardware

--- tests/test_ephys_task_extraction.py
+++ tests/test_ephys_task_extraction.py
@@ -34,39 +34,52 @@ ALIGN_BPOD_FPGA_FILES = [
 ]
 
 
+
 class TestEphysTaskExtraction(unittest.TestCase):
 
-    def test_task_extraction_output(self):
-        self.root_folder = Path('/mnt/s0/Data/IntegrationTests/ephys/choice_world_init')
+    def setUp(self) -> None:
+        self.root_folder = Path("/datadisk/Data/IntegrationTests/ephys")
         if not self.root_folder.exists():
             return
-        self.sessions = [f.parent for f in self.root_folder.rglob('raw_ephys_data')]
+
+    def test_task_extraction_output(self):
+        init_folder = self.root_folder.joinpath("choice_world_init")
+        self.sessions = [f.parent for f in init_folder.rglob('raw_ephys_data')]
         for session_path in self.sessions:
-            alf_path = session_path.joinpath('alf')
-            shutil.rmtree(alf_path, ignore_errors=True)
-            sync, chmap = ephys_fpga._get_main_probe_sync(session_path, bin_exists=False)
-            bpod_behaviour = ephys_trials.extract_all(session_path, save=True)
-            # check that the output is complete
-            for f in BPOD_FILES:
-                self.assertTrue(alf_path.joinpath(f).exists())
-            fpga_behaviour = ephys_fpga.extract_behaviour_sync(sync, chmap=chmap,
-                                                               output_path=alf_path, save=True)
-            # check that the output is complete
-            for f in FPGA_FILES:
-                self.assertTrue(alf_path.joinpath(f).exists())
-            # checks that all matrices in fpga_behaviour have the same number of trials
-            self.assertTrue(np.size(np.unique([fpga_behaviour[k].shape[0]
-                                               for k in fpga_behaviour])) == 1)
-            # check dimensions after alf load
-            self.assertEqual(alf.io.check_dimensions(alf.io.load_object(alf_path, '_ibl_trials'))
-                             , 0)
-            # all trials have either valve open or error tone in and are mutually exclusive
-            self.assertTrue(np.all(np.isnan(fpga_behaviour['valve_open']
-                                            * fpga_behaviour['error_tone_in'])))
-            self.assertTrue(np.all(np.logical_xor(np.isnan(fpga_behaviour['valve_open']),
-                                                  np.isnan(fpga_behaviour['error_tone_in']))))
-            # align with the bpod
-            ephys_fpga.align_with_bpod(session_path)
-            for f in ALIGN_BPOD_FPGA_FILES:
-                self.assertTrue(alf_path.joinpath(f).exists())
-            shutil.rmtree(alf_path, ignore_errors=True)
+            self._task_extraction_assertions(session_path)
+
+    def test_task_extraction_bpod_events_mismatch(self):
+        init_folder = self.root_folder.joinpath("ephys_choice_world_task")
+        self.sessions = [f.parent for f in init_folder.rglob('raw_ephys_data')]
+        for session_path in self.sessions:
+            self._task_extraction_assertions(session_path)
+
+    def _task_extraction_assertions(self, session_path):
+        alf_path = session_path.joinpath('alf')
+        shutil.rmtree(alf_path, ignore_errors=True)
+        sync, chmap = ephys_fpga._get_main_probe_sync(session_path, bin_exists=False)
+        bpod_behaviour = ephys_trials.extract_all(session_path, save=True)
+        # check that the output is complete
+        for f in BPOD_FILES:
+            self.assertTrue(alf_path.joinpath(f).exists())
+        fpga_behaviour = ephys_fpga.extract_behaviour_sync(sync, chmap=chmap,
+                                                           output_path=alf_path, save=True)
+        # check that the output is complete
+        for f in FPGA_FILES:
+            self.assertTrue(alf_path.joinpath(f).exists())
+        # checks that all matrices in fpga_behaviour have the same number of trials
+        self.assertTrue(np.size(np.unique([fpga_behaviour[k].shape[0]
+                                           for k in fpga_behaviour])) == 1)
+        # all trials have either valve open or error tone in and are mutually exclusive
+        self.assertTrue(np.all(np.isnan(fpga_behaviour['valve_open']
+                                        * fpga_behaviour['error_tone_in'])))
+        self.assertTrue(np.all(np.logical_xor(np.isnan(fpga_behaviour['valve_open']),
+                                              np.isnan(fpga_behaviour['error_tone_in']))))
+        # align with the bpod
+        ephys_fpga.align_with_bpod(session_path)
+        for f in ALIGN_BPOD_FPGA_FILES:
+            self.assertTrue(alf_path.joinpath(f).exists())
+        # check dimensions after alf load
+        self.assertEqual(alf.io.check_dimensions(alf.io.load_object(alf_path, '_ibl_trials'))
+                         , 0)
+        shutil.rmtree(alf_path, ignore_errors=True)
