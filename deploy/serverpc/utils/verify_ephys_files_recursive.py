c27b8ea398c786ce8f81992a49536568cf2f5141
olivier <olivier.winter@hotmail.fr>
2019-12-18 14:04:30 +0000

    full SHA1 hash recompute option for recursive check ephys files

--- deploy/serverpc/utils/verify_ephys_files_recursive.py
+++ deploy/serverpc/utils/verify_ephys_files_recursive.py
@@ -7,7 +7,7 @@ from ibllib.io import spikeglx
 _logger = logging.getLogger('ibllib')
 
 
-def check_ephys_file(root_path):
+def check_ephys_file(root_path, hash=False):
     root_path = Path(root_path)
     efiles = spikeglx.glob_ephys_files(root_path)
     for ef in efiles:
@@ -16,6 +16,10 @@ def check_ephys_file(root_path):
                 continue
             try:
                 sr = spikeglx.Reader(ef[lab])
+                if hash:
+                    ok = sr.verify_hash()
+                    if not ok:
+                        raise ValueError("hashes don't match")
                 _logger.info(f"PASS {ef[lab]}")
             except(Exception) as e:
                 _logger.error(f"FAILED {ef[lab]} is corrupt !!")
@@ -24,7 +28,9 @@ def check_ephys_file(root_path):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check ephys files')
     parser.add_argument('folder', help='Any folder')
+    parser.add_argument('--hash', dest='hash', help='Computes hash and verify with meta-data',
+                        const=True, default=False, nargs='?')
     args = parser.parse_args()  # returns data from the options specified (echo)
     root_path = Path(args.folder)
     assert(root_path.exists())
-    check_ephys_file(root_path)
+    check_ephys_file(root_path, hash=args.hash)
