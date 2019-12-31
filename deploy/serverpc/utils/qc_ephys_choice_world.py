740ed84c88dc639a310b5f5f4e4cbe3ad4a684c1
olivier <olivier.winter@hotmail.fr>
2019-12-31 18:12:18 +0000

    training wheel check against ephys wheel

--- deploy/serverpc/utils/qc_ephys_choice_world.py
+++ deploy/serverpc/utils/qc_ephys_choice_world.py
@@ -8,6 +8,7 @@ from brainbox.core import Bunch
 
 from ibllib.ephys import ephysqc
 from ibllib.io.extractors import ephys_fpga, training_wheel, ephys_trials, biased_trials
+import ibllib.io.raw_data_loaders as rawio
 
 # To log errors :
 import logging
@@ -43,35 +44,64 @@ def qc_ephys_session(eid, display=True):
     temp_alf_folder = sess_path.joinpath('fpga_test', 'alf')
     temp_alf_folder.mkdir(parents=True, exist_ok=True)
 
+    raw_trials = rawio.load_data(sess_path)
+    tmax = raw_trials[-1]['behavior_data']['States timestamps']['exit_state'][0][-1] + 60
 
     sync, chmap = ephys_fpga._get_main_probe_sync(sess_path, bin_exists=False)
     bpod_trials = ephys_trials.extract_all(sess_path, output_path=temp_alf_folder, save=True)
     # check that the output is complete
-    fpga_trials = ephys_fpga.extract_behaviour_sync(sync, output_path=temp_alf_folder,
+    fpga_trials = ephys_fpga.extract_behaviour_sync(sync, output_path=temp_alf_folder, tmax=tmax,
                                                     chmap=chmap, save=True, display=display)
     # align with the bpod
-    ephys_fpga.align_with_bpod(temp_alf_folder.parent)
+    bpod2fpga = ephys_fpga.align_with_bpod(temp_alf_folder.parent)
     alf_trials = alf.io.load_object(temp_alf_folder, '_ibl_trials')
 
     # do the QC
     qcs, qct = ephysqc.qc_fpga_task(fpga_trials, alf_trials)
+    if False:
+        """
+        Some sanity checks that are used as testing (not QC)
+        """
+        # checks that all matrices in qc_fpga_task have the same number of trials
+        assert (np.size(np.unique([fpga_trials[k].shape[0] for k in fpga_trials])) == 1)
+        # all trials have either valve open or error tone in and are mutually exclusive
+        assert np.all(np.isnan(fpga_trials['valve_open'] * fpga_trials['error_tone_in']))
+        assert np.all(np.logical_xor(np.isnan(fpga_trials['valve_open']),
+                                     np.isnan(fpga_trials['error_tone_in'])))
 
-    """
-    Some sanity checks that are used as testing (not QC)
-    """
-    # checks that all matrices in qc_fpga_task have the same number of trials
-    assert (np.size(np.unique([fpga_trials[k].shape[0] for k in fpga_trials])) == 1)
-    # all trials have either valve open or error tone in and are mutually exclusive
-    assert np.all(np.isnan(fpga_trials['valve_open'] * fpga_trials['error_tone_in']))
-    assert np.all(np.logical_xor(np.isnan(fpga_trials['valve_open']),
-                                 np.isnan(fpga_trials['error_tone_in'])))
+    # do the wheel part
+    bpod_wheel = training_wheel.get_wheel_data(sess_path, save=False)
+    fpga_wheel = ephys_fpga.extract_wheel_sync(sync, chmap=chmap, save=False)
+
+    if display:
+
+        t0 = max(np.min(bpod2fpga(bpod_wheel['re_ts'])), np.min(fpga_wheel['re_ts']))
+        dy = np.interp(t0, fpga_wheel['re_ts'], fpga_wheel['re_pos']) - np.interp(
+            t0, bpod2fpga(bpod_wheel['re_ts']), bpod_wheel['re_pos'])
+
+        fix, axes = plt.subplots(nrows=2, sharex='all', sharey='all')
+        # axes[0].plot(t, pos), axes[0].title.set_text('Extracted')
+        axes[0].plot(bpod2fpga(bpod_wheel['re_ts']), bpod_wheel['re_pos'] + dy)
+        axes[0].plot(fpga_wheel['re_ts'], fpga_wheel['re_pos'])
+        axes[0].title.set_text('FPGA')
+        axes[1].plot(bpod2fpga(bpod_wheel['re_ts']), bpod_wheel['re_pos'] + dy)
+        axes[1].title.set_text('Bpod')
+
+
+
+
+
+
+
 
 eids = one.search(task_protocol='ephyschoice', dataset_types=dtypes)
 
-OFFSET = 1  # extract the last trial when there is the passive protocol trailing...
+OFFSET = 6  # extract the last trial when there is the passive protocol trailing...
 for i, eid in enumerate(eids):
     if i < OFFSET:
         continue
     qc_ephys_session(eid, display=True)
+    break
 
-# '53738f95-bd08-4d9d-9133-483fdb19e8da' valve_open / error_tone in: check channel maps as it's 3A
+# '53738f95-bd08-4d9d-9133-483fdb19e8da' passive stimulus issue: TODO integration test
+#  'c6d5cea7-e1c4-48e1-8898-78e039fabf2b' trial 470 (t 2048-2052) has no feedback
--- tests/test_wheel_extraction.py
+++ tests/test_wheel_extraction.py
@@ -3,37 +3,38 @@ import numpy as np
 import unittest
 import shutil
 import scipy.interpolate
+import matplotlib.pyplot as plt
 
 import alf.io
 from ibllib.io.extractors import ephys_fpga, training_wheel, ephys_trials
 from ibllib.io.raw_data_loaders import load_settings
 
+DISPLAY = False
 
-def compare_wheel_fpga_behaviour(session_path):
+
+def compare_wheel_fpga_behaviour(session_path, display=DISPLAY):
     alf_path = session_path.joinpath('alf')
     shutil.rmtree(alf_path, ignore_errors=True)
     sync_path = session_path.joinpath(r'raw_ephys_data')
     sync = alf.io.load_object(sync_path, '_spikeglx_sync', short_keys=True)
     chmap = ephys_fpga.CHMAPS['3B']['nidq']
     fpga_wheel = ephys_fpga.extract_wheel_sync(sync, chmap=chmap, save=False)
-    bpod_wheel = training_wheel.get_wheel_data(session_path, save=False)
+    bpod_wheel = training_wheel.get_wheel_data(session_path, save=False, display=display)
     ephys_trials.extract_all(session_path, output_path=alf_path, save=True)
     ephys_fpga.extract_behaviour_sync(sync, output_path=alf_path, chmap=chmap, save=True)
-    dt = ephys_fpga.align_with_bpod(session_path)
+    bpod2fpga = ephys_fpga.align_with_bpod(session_path)
     # resample both traces to the same rate and compute correlation coeff
-    tmin = max([np.min(fpga_wheel['re_ts']), np.min(bpod_wheel['re_ts'] + dt)])
-    tmax = min([np.max(fpga_wheel['re_ts']), np.max(bpod_wheel['re_ts'] + dt)])
-    tscale = np.linspace(tmin, tmax, 500)
-    fw = scipy.interpolate.interp1d(fpga_wheel['re_ts'], fpga_wheel['re_pos'])(tscale)
-    bw = scipy.interpolate.interp1d(bpod_wheel['re_ts'] + dt, bpod_wheel['re_pos'])(tscale)
-    import matplotlib.pyplot as plt
-    plt.figure()
-    plt.plot(fpga_wheel['re_ts'], fpga_wheel['re_pos'])
-    plt.plot(bpod_wheel['re_ts'] + dt, bpod_wheel['re_pos'])
-    plt.plot(tscale, fw)
-    plt.plot(tscale, bw)
-    # test that the extractions match
-    return fpga_wheel, bpod_wheel, fw, bw
+    bpod_wheel['re_ts'] = bpod2fpga(bpod_wheel['re_ts'])
+    tmin = max([np.min(fpga_wheel['re_ts']), np.min(bpod_wheel['re_ts'])])
+    tmax = min([np.max(fpga_wheel['re_ts']), np.max(bpod_wheel['re_ts'])])
+    wheel = {'tscale': np.arange(tmin, tmax, 0.01)}
+    wheel['fpga'] = scipy.interpolate.interp1d(fpga_wheel['re_ts'], fpga_wheel['re_pos'])(wheel['tscale'])
+    wheel['bpod'] = scipy.interpolate.interp1d(bpod_wheel['re_ts'], bpod_wheel['re_pos'])(wheel['tscale'])
+    if display:
+        plt.figure()
+        plt.plot(fpga_wheel['re_ts'] - bpod2fpga(0), fpga_wheel['re_pos'], '*')
+        plt.plot(bpod_wheel['re_ts'] - bpod2fpga(0), bpod_wheel['re_pos'], '.')
+    return fpga_wheel, bpod_wheel, wheel
 
 
 class TestWheelExtractionSimpleEphys(unittest.TestCase):
@@ -45,8 +46,8 @@ class TestWheelExtractionSimpleEphys(unittest.TestCase):
             return
 
     def test_three_clockwise_revolutions_fpga(self):
-        fpga_wheel, bpod_wheel, fw, bw = compare_wheel_fpga_behaviour(self.session_path)
-        self.assertTrue(np.all(np.abs(fw - bw) < 0.1))
+        fpga_wheel, bpod_wheel, wheel = compare_wheel_fpga_behaviour(self.session_path)
+        self.assertTrue(np.all(np.abs(wheel['fpga'] - wheel['bpod']) < 0.1))
         # test that the units are in radians: we expect around 9 revolutions clockwise
         self.assertTrue(0.95 < fpga_wheel['re_pos'][-1] / -(2 * 3.14 * 9) < 1.05)
 
@@ -59,11 +60,16 @@ class TestWheelExtractionSessionEphys(unittest.TestCase):
             return
 
     def test_wheel_extraction_session(self):
-        fpga_wheel, bpod_wheel, fw, bw = compare_wheel_fpga_behaviour(self.session_path)
-        return
-        self.assertTrue(np.all(np.abs(fw - bw) < 0.1))
-        # test that the units are in radians: we expect around 9 revolutions clockwise
-        self.assertTrue(0.95 < fpga_wheel['re_pos'][-1] / -(2 * 3.14 * 9) < 1.05)
+        fpga_wheel, bpod_wheel, wheel = compare_wheel_fpga_behaviour(self.session_path)
+        # makes sure that the HF component matches
+        from scipy.signal import butter, filtfilt
+        b, a = butter(3, 0.0001, btype='high', analog=False)
+        fpga = filtfilt(b, a, wheel['fpga'])
+        bpod = filtfilt(b, a, wheel['bpod'])
+        # plt.figure()
+        # plt.plot(wheel['tscale'], fpga)
+        # plt.plot(wheel['tscale'], bpod)
+        self.assertTrue(np.all(np.abs(fpga - bpod < 0.1)))
 
 
 class TestWheelExtractionTraining(unittest.TestCase):
