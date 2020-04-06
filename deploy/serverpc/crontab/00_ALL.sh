3f6e59d712300c1598372f242e86a12961fd2042
olivier <olivier.winter@hotmail.fr>
2020-04-06 15:12:58 +0100

    crontab update for ephys video compression

--- deploy/serverpc/crontab/00_ALL.sh
+++ deploy/serverpc/crontab/00_ALL.sh
@@ -8,4 +8,4 @@ cd ~/Documents/PYTHON/iblscripts/deploy/serverpc/crontab
 ./20_extract_ephys.sh; ./02_register.sh
 ./03_compress_videos.sh; ./02_register.sh &
 ./04_audio_training.sh; ./02_register.sh &
-./21_raw_ephys_qc.sh; ./23_compress_ephys.sh; ./02_register.sh &
+./21_raw_ephys_qc.sh; ./23_compress_ephys.sh; ./27_compress_ephys_videos.sh; ./02_register.sh &
