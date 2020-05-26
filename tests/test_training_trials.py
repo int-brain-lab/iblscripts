ba3cbe69b0fbb5747d53e25116421784ea9f1474
olivier <olivier.winter@hotmail.fr>
2020-05-26 16:02:56 +0100

    Training extraction tests

---
+++ tests/test_training_trials.py
@@ -0,0 +1,62 @@
+import unittest
+from pathlib import Path
+import shutil
+import tempfile
+
+import numpy as np
+
+from ibllib.misc import version
+from ibllib.pipes.training_preprocessing import TrainingTrials
+import ibllib.io.raw_data_loaders as rawio
+import alf.io
+
+PATH_TESTS = Path('/mnt/s0/Data/IntegrationTests')
+INIT_FOLDER = PATH_TESTS.joinpath('training')
+TRIAL_KEYS_ge5 = ['goCue_times', 'probabilityLeft', 'intervals', 'stimOnTrigger_times',
+                  'goCueTrigger_times', 'response_times', 'feedbackType', 'contrastLeft',
+                  'feedback_times', 'rewardVolume', 'included', 'choice', 'contrastRight',
+                  'stimOn_times']
+TRIAL_KEYS_lt5 = ['goCue_times', 'probabilityLeft', 'intervals', 'itiDuration',
+                  'goCueTrigger_times', 'response_times', 'feedbackType', 'contrastLeft',
+                  'feedback_times', 'rewardVolume', 'choice', 'contrastRight', 'stimOn_times']
+WHEEL_KEYS = ['position', 'timestamps']
+
+
+class TestSessions(unittest.TestCase):
+
+    def test_trials_extraction(self):
+        if not INIT_FOLDER.exists():
+            return
+        # extract all sessions
+        with tempfile.TemporaryDirectory() as tdir:
+            subjects_path = Path(tdir).joinpath('Subjects')
+            shutil.copytree(INIT_FOLDER, subjects_path)
+            for fil in subjects_path.rglob('_iblrig_taskData.raw*.jsonable'):
+                session_path = fil.parents[1]
+                job = TrainingTrials(session_path)
+                job.run()
+                # load camera timestamps
+                lc = alf.io.load_object(session_path / 'alf', '_ibl_leftCamera')
+                self.assertTrue(np.all(np.diff(lc.times) > 0))
+                # check the trials objects
+                trials = alf.io.load_object(session_path / 'alf', '_ibl_trials')
+                self.assertTrue(alf.io.check_dimensions(trials) == 0)
+                settings = rawio.load_settings(session_path)
+                if version.ge(settings['IBLRIG_VERSION_TAG'], '5.0.0'):
+                    tkeys = TRIAL_KEYS_ge5
+                else:
+                    tkeys = TRIAL_KEYS_lt5
+                self.assertTrue(set(trials.keys()) == set(tkeys))
+                # check the wheel object if the extraction didn't fail
+                if job.status != -1:
+                    wheel = alf.io.load_object(session_path / 'alf', '_ibl_wheel')
+                    self.assertTrue(alf.io.check_dimensions(wheel) == 0)
+                    self.assertTrue(set(wheel.keys()) == set(WHEEL_KEYS))
+                    self.assertTrue(np.all(np.diff(lc.times) > 0))
+            """
+            For this session only the downgoing front of a trial was detected, resulting in an error
+            for the gocuetime. The fix was to extract the downgoing front and subtract 100ms.
+            """
+            session_path = subjects_path / "CSHL_007/2019-07-31/001"
+            trials = alf.io.load_object(session_path / 'alf', '_ibl_trials')
+            self.assertTrue(np.all(np.logical_not(np.isnan(trials.goCue_times))))
--- tests/test_wheel_extraction.py
+++ tests/test_wheel_extraction.py
@@ -4,6 +4,7 @@ import unittest
 import shutil
 import logging
 
+from scipy.signal import butter, filtfilt
 import scipy.interpolate
 import matplotlib.pyplot as plt
 
@@ -19,26 +20,26 @@ def compare_wheel_fpga_behaviour(session_path, display=DISPLAY):
     alf_path = session_path.joinpath('alf')
     shutil.rmtree(alf_path, ignore_errors=True)
     sync, chmap = ephys_fpga._get_main_probe_sync(session_path, bin_exists=False)
-    fpga_wheel = ephys_fpga.extract_wheel_sync(sync, chmap=chmap)
-    bpod_wheel = training_wheel.get_wheel_position(session_path, display=display)
+    fpga_t, fpga_pos = ephys_fpga.extract_wheel_sync(sync, chmap=chmap)
+    bpod_t, bpod_pos = training_wheel.get_wheel_position(session_path, display=display)
     data, _ = ephys_fpga.extract_all(session_path)
-
     bpod2fpga = scipy.interpolate.interp1d(data['intervals_bpod'][:, 0], data['intervals'][:, 0],
                                            fill_value="extrapolate")
     # resample both traces to the same rate and compute correlation coeff
-    bpod_wheel['re_ts'] = bpod2fpga(bpod_wheel['re_ts'])
-    tmin = max([np.min(fpga_wheel['re_ts']), np.min(bpod_wheel['re_ts'])])
-    tmax = min([np.max(fpga_wheel['re_ts']), np.max(bpod_wheel['re_ts'])])
+    bpod_t = bpod2fpga(bpod_t)
+    tmin = max([np.min(fpga_t), np.min(bpod_t)])
+    tmax = min([np.max(fpga_t), np.max(bpod_t)])
     wheel = {'tscale': np.arange(tmin, tmax, 0.01)}
     wheel['fpga'] = scipy.interpolate.interp1d(
-        fpga_wheel['re_ts'], fpga_wheel['re_pos'])(wheel['tscale'])
+        fpga_t, fpga_pos)(wheel['tscale'])
     wheel['bpod'] = scipy.interpolate.interp1d(
-        bpod_wheel['re_ts'], bpod_wheel['re_pos'])(wheel['tscale'])
+        bpod_t, bpod_pos)(wheel['tscale'])
     if display:
         plt.figure()
-        plt.plot(fpga_wheel['re_ts'] - bpod2fpga(0), fpga_wheel['re_pos'], '*')
-        plt.plot(bpod_wheel['re_ts'] - bpod2fpga(0), bpod_wheel['re_pos'], '.')
-    return fpga_wheel, bpod_wheel, wheel
+        plt.plot(fpga_t - bpod2fpga(0), fpga_pos, '*')
+        plt.plot(bpod_t - bpod2fpga(0), bpod_pos, '.')
+    raw_wheel = {'fpga_t': fpga_t, 'fpga_pos': fpga_pos, 'bpod_t': bpod_t, 'bpod_pos': bpod_pos}
+    return raw_wheel, wheel
 
 
 class TestWheelExtractionSimpleEphys(unittest.TestCase):
@@ -49,10 +50,10 @@ class TestWheelExtractionSimpleEphys(unittest.TestCase):
             return
 
     def test_three_clockwise_revolutions_fpga(self):
-        fpga_wheel, bpod_wheel, wheel = compare_wheel_fpga_behaviour(self.session_path)
+        raw_wheel, wheel = compare_wheel_fpga_behaviour(self.session_path)
         self.assertTrue(np.all(np.abs(wheel['fpga'] - wheel['bpod']) < 0.1))
         # test that the units are in radians: we expect around 9 revolutions clockwise
-        self.assertTrue(0.95 < fpga_wheel['re_pos'][-1] / -(2 * 3.14 * 9) < 1.05)
+        self.assertTrue(0.95 < raw_wheel['fpga_pos'][-1] / -(2 * 3.14 * 9) < 1.05)
 
 
 class TestWheelExtractionSessionEphys(unittest.TestCase):
@@ -66,9 +67,8 @@ class TestWheelExtractionSessionEphys(unittest.TestCase):
     def test_wheel_extraction_session(self):
         for session_path in self.sessions:
             _logger.info(f"EPHYS: {session_path}")
-            fpga_wheel, bpod_wheel, wheel = compare_wheel_fpga_behaviour(session_path)
+            _, wheel = compare_wheel_fpga_behaviour(session_path)
             # makes sure that the HF component matches
-            from scipy.signal import butter, filtfilt
             b, a = butter(3, 0.0001, btype='high', analog=False)
             fpga = filtfilt(b, a, wheel['fpga'])
             bpod = filtfilt(b, a, wheel['bpod'])
@@ -89,5 +89,5 @@ class TestWheelExtractionTraining(unittest.TestCase):
         for rbf in self.root_path.rglob('raw_behavior_data'):
             session_path = alf.io.get_session_path(rbf)
             _logger.info(f"TRAINING: {session_path}")
-            bpod_wheel = training_wheel.get_wheel_position(session_path)
-            self.assertTrue(bpod_wheel['re_ts'].size)
+            bpod_t, _ = training_wheel.get_wheel_position(session_path)
+            self.assertTrue(bpod_t.size)
