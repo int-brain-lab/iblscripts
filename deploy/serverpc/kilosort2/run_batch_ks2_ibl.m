a606f0c47b0ff11dce85d07378a69750c1c43ec8
olivier <olivier.winter@hotmail.fr>
2020-07-29 13:29:49 +0100

    wip scripts ks2

--- deploy/serverpc/kilosort2/run_batch_ks2_ibl.m
+++ deploy/serverpc/kilosort2/run_batch_ks2_ibl.m
@@ -1,4 +1,8 @@
-function run_batch_ks2_ibl
+function run_batch_ks2_ibl(rootH, dry)
+% run_batch_ks2_ibl(rootH, dry)
+% rootH: swap directory (defaults to '/mnt/h0/') SSD drive that will be used to host temporary binary files for KS2
+% dry: bool (defaults to false). If true, just lists the probe files that would be spike sorted by the script
+% Author: Anne Urai
 
 %% SET PATHS - also for vizualisation
 addpath(genpath('~/Documents/MATLAB/Kilosort2')) % path to kilosort folder
@@ -6,100 +10,95 @@ addpath('~/Documents/MATLAB/npy-matlab/npy-matlab');
 addpath(genpath('/home/ibladmin/Documents/MATLAB/spikes'));
 addpath('/home/ibladmin/Documents/PYTHON/iblscripts/deploy/serverpc/kilosort2');
 
-rootH = '/mnt/h0/';
+if nargin <= 0, rootH = '/mnt/h0/'; end
+if nargin <= 0, dry = false; end
+
+root = '/mnt/s0/Data/Subjects';
 figure_dir = '~/Documents/figures';
 set(groot, 'defaultaxesfontsize', 6);
 
 %% ========== set path to files
 % rootZ is the directory containing the raw AP traces, one probe per folder
 % rootH is the scratch directory, SSD drive, to memmap binary data
-ROOTZ = {...
-    '/mnt/s0/Data/Subjects/CSHL049/2020-01-09/001/raw_ephys_data/probe00',... # running
-   '/mnt/s0/Data/Subjects/CSHL049/2020-01-09/001/raw_ephys_data/probe01',... # running
-    };
+allfolders = dir(sprintf('%s/*/20*/0*/raw_ephys_data/probe*/*.ap.cbin', root));
+[~, idx] = sort([allfolders(:).datenum], 'descend'); % start with most recent
+allfolders = allfolders(idx);
+
+to_spikesort = {};
+for f = 1:length(allfolders),
+    spikesorting_present = dir(sprintf('%s/spike_sorting_ks2.log', allfolders(f).folder));
+    if isempty(spikesorting_present),
+        to_spikesort{end+1} = allfolders(f).folder;
+    end
+end
+
+disp(to_spikesort');
+if dry, return, end
 
 %% ========== run spike sorting
-for m = 1:length(ROOTZ)
-    
+for m = 1:length(to_spikesort)
+
     close all;
-    rootZ = ROOTZ{m};
+    rootZ = to_spikesort{m};
     a = strsplit(rootZ, '/');
-    
-    % ==================== % wait for the transfer to finish
-    while ~exist(sprintf('%s/spike_sorting.flag', rootZ), 'file')
-        pause(60);
-    end
+    fig_name = sprintf('%s_%s_%s_%s', a{6}, a{7}, a{8}, a{10});
     disp(rootZ);
-    
-    %% ========== %
-    if ~exist(sprintf('%s/whitening_mat.npy', rootZ), 'file')
-        
-        % ==================== % run KS2
-        run_ks2_ibl(rootZ, rootH);
-        
-        % ==================== % save the ks2 figures
-        figure(1); tightfig;
-        print(gcf, '-dpdf', sprintf('%s/%s_%s_%s_%s_ksFig1.pdf', figure_dir, a{6}, a{7}, a{8}, a{10}));
-        figure(2); tightfig;
-        print(gcf, '-dpdf', sprintf('%s/%s_%s_%s_%s_ksFig2.pdf', figure_dir, a{6}, a{7}, a{8}, a{10}));
+
+
+
+
+    %% ==================== % run KS2
+    run_ks2_ibl(scratch_dir, rootH);
+
+    %% ========== move files to correct location
+    results_files = dir('/mnt/h0/temp/');
+    for ff = 1:length(results_files),
+        if ~contains(results_files(ff).name, 'ap.bin') && ~results_files(ff).isdir,
+            copyfile(fullfile(results_files(ff).folder, results_files(ff).name), ...
+                fullfile(rootZ, results_files(ff).name));
+        end
     end
-    
-    %% ===================== plots: from https://github.com/cortex-lab/spikes/
-    close all;
-    
-    % plot drift map
-    [spikeTimes, spikeAmps, spikeDepths, spikeSites] = ksDriftmap(rootZ);
-    clf; plotDriftmap(spikeTimes, spikeAmps, spikeDepths);
-    title(sprintf('%s/%s/%s/%s', a{6}, a{7}, a{8}, a{10}), 'interpreter', 'none');
-    axis tight; axisNotSoTight; tightfig;
-    print(gcf, '-dpdf', sprintf('%s/%s_%s_%s_%s_ksDriftmap.pdf', figure_dir, a{6}, a{7}, a{8}, a{10}));
-    
-    % spike amplitudes
-    [pdfs, cdfs] = computeWFampsOverDepth(spikeAmps, spikeDepths, 0:30:min(max(spikeAmps),800), ...
-        0:40:3840, spikeTimes(end));
-    plotWFampCDFs(pdfs, cdfs, 0:30:min(max(spikeAmps),800), 0:40:3840);
-    print(gcf, '-dpdf', sprintf('%s/%s_%s_%s_%s_splikeAmplitudes.pdf', figure_dir, a{6}, a{7}, a{8}, a{10}));
-    
-    % LFP
-    lfpD = dir(fullfile(rootZ, '*.lf.bin')); % LFP file from spikeGLX specifically
-    if ~isempty(lfpD),
-        lfpFilename = fullfile(rootZ, lfpD(1).name);
-        lfpFs = 2500;  % neuropixels phase3a
-        nChansInFile = 385;  % neuropixels phase3a, from spikeGLX
-        [lfpByChannel, allPowerEst, F, allPowerVar] = ...
-            lfpBandPower(lfpFilename, lfpFs, nChansInFile, []);
-        chanMap = readNPY(fullfile(rootZ, 'channel_map.npy'));
-        nC = length(chanMap);
-        allPowerEst = allPowerEst(:,chanMap+1)'; % now nChans x nFreq
-        % plot LFP power
-        dispRange = [0 100]; % Hz
-        marginalChans = [10:50:nC];
-        freqBands = {[1.5 4], [4 10], [10 30], [30 80], [80 200]};
-        plotLFPpower(F, allPowerEst, dispRange, marginalChans, freqBands);
-        tightfig;
-        print(gcf, '-dpdf', sprintf('%s/%s_%s_%s_%s_LFP.pdf', figure_dir, a{6}, a{7}, a{8}, a{10}));
+
+    %% ==================== % save the ks2 figures
+    try
+        figure(1);
+        print(gcf, '-dpdf', sprintf('%s/%s_ksFig1.pdf', figure_dir, fig_name));
+        figure(2);
+        print(gcf, '-dpdf', sprintf('%s/%s_ksFig2.pdf', figure_dir, fig_name));
+
+        % ===================== plots: from https://github.com/cortex-lab/spikes/
+        % plot drift map
+        [spikeTimes, spikeAmps, spikeDepths, spikeSites] = ksDriftmap(rootZ);
+        clf; plotDriftmap(spikeTimes, spikeAmps, spikeDepths);
+        title(sprintf('%s/%s/%s/%s', a{6}, a{7}, a{8}, a{10}), 'interpreter', 'none');
+        axis tight;
+        print(gcf, '-dpdf', sprintf('%s/%s_ksDriftmap.pdf', figure_dir, fig_name));
+
+        % spike amplitudes
+        [pdfs, cdfs] = computeWFampsOverDepth(spikeAmps, spikeDepths, 0:30:min(max(spikeAmps),800), ...
+            0:40:3840, spikeTimes(end));
+        plotWFampCDFs(pdfs, cdfs, 0:30:min(max(spikeAmps),800), 0:40:3840);
+        print(gcf, '-dpdf', sprintf('%s/%s_splikeAmplitudes.pdf', figure_dir, fig_name));
+
+        % LFP
+        lfpD = dir(fullfile(rootZ, '*.lf.cbin')); % LFP file from spikeGLX specifically
+        if ~isempty(lfpD),
+            lfpFilename = fullfile(rootZ, lfpD(1).name);
+            lfpFs = 2500;  % neuropixels phase3a
+            nChansInFile = 385;  % neuropixels phase3a, from spikeGLX
+            [lfpByChannel, allPowerEst, F, allPowerVar] = ...
+                lfpBandPower(lfpFilename, lfpFs, nChansInFile, []);
+            chanMap = readNPY(fullfile(rootZ, 'channel_map.npy'));
+            nC = length(chanMap);
+            allPowerEst = allPowerEst(:,chanMap+1)'; % now nChans x nFreq
+            % plot LFP power
+            dispRange = [0 100]; % Hz
+            marginalChans = [10:50:nC];
+            freqBands = {[1.5 4], [4 10], [10 30], [30 80], [80 200]};
+            plotLFPpower(F, allPowerEst, dispRange, marginalChans, freqBands);
+            print(gcf, '-dpdf', sprintf('%s/%s_LFP.pdf', figure_dir, fig_name));
+        end
+    catch
+        warning('Could not plot figures. Do you have the spikes repo from the cortexlab github on your path?');
     end
-    
 end
-
-%% ========== start extraction
-
-for m = 1:length(ROOTZ)
-    rootZ = ROOTZ{m};
-    a = strsplit(rootZ, '/');
-    pathnames{m} = fullfile(a{1:end-2});
-end
-
-% now take only the unique ones - do once for each session only
-pathnames = unique(pathnames);
-
-for p = 1:length(pathnames),
-    % run the python command that will do the extraction
-    command = ['cd ~/Documents/PYTHON/iblscripts/deploy/serverpc/utils; ' ...
-        'source ~/Documents/PYTHON/envs/iblenv/bin/activate; pwd; ' ...
-        'python extract_ephys_manual.py /' pathnames{p}];
-    [status,cmdout] = system(command)
-end
-
-end
-
--- deploy/serverpc/kilosort2/run_ks2_ibl.m
+++ deploy/serverpc/kilosort2/run_ks2_ibl.m
@@ -7,54 +7,24 @@ function run_ks2_ibl(rootZ, rootH, varargin)
 % rootH = '/mnt/h0';
 % run_ks2_ibl(rootZ, rootH)
 
+%% 1) Set paths and get ks2 commit hash
+addpath(genpath('~/Documents/MATLAB/Kilosort2')) % path to kilosort folder
+addpath('~/Documents/MATLAB/npy-matlab/npy-matlab')
+[~, hash] = unix('git --git-dir ~/Documents/MATLAB/Kilosort2/.git rev-parse --verify HEAD');
+
+return
+%% 2) Parse input arguments
+[dir_pattern, channel_map_file] = deal([]);
 p=inputParser;
 p.addParameter('dir_pattern', '*.ap.bin');
 p.addParameter('channel_map_file','~/Documents/MATLAB/Kilosort2/configFiles/neuropixPhase3A_kilosortChanMap.mat');
 p.parse(varargin{:}); field = fieldnames(p.Results) ;
 for r = 1 : length(field), eval([field{r} '= p.Results.(field{r}) ; ']) ; end
 
-%% SET PATHS
-addpath(genpath('~/Documents/MATLAB/Kilosort2')) % path to kilosort folder
-addpath('~/Documents/MATLAB/npy-matlab/npy-matlab')
-[~, hash] = unix('git --git-dir ~/Documents/MATLAB/Kilosort2/.git rev-parse --verify HEAD');
+%% 5) get IBL params
+ops = ibl_ks2_params;
 
-%% PARAMS
-ops.commitHash = strip(hash);
-ops.chanMap = channel_map_file;
-ops.fs = 30000;   % sample rate
-ops.fshigh = 300;    % frequency for high pass filtering (150)
-ops.minfr_goodchannels = 0;  % minimum firing rate on a "good" channel (0 to skip)
-ops.Th = [10 4];   % threshold on projections (like in Kilosort1, can be different for last pass like [10 4])
-ops.lam = 10;  % how important is the amplitude penalty (like in Kilosort1, 0 means not used, 10 is average, 50 is a lot)
-ops.AUCsplit = 0.9; % splitting a cluster at the end requires at least this much isolation for each sub-cluster (max = 1)
-ops.minFR = 1/50; % minimum spike rate (Hz), if a cluster falls below this for too long it gets removed
-ops.momentum = [20 400]; % number of samples to average over (annealed from first to second value)
-ops.sigmaMask = 30; % spatial constant in um for computing residual variance of spike
-ops.ThPre = 8;  % threshold crossings for pre-clustering (in PCA projection space)
-ops.CAR = true;  % Common Average Referencing (median)
-% DANGER ZONE: changing these settings can lead to fatal errors
-% options for determining PCs
-ops.spkTh           = -6;      % spike threshold in standard deviations (-6)
-ops.reorder         = 1;       % whether to reorder batches for drift correction.
-ops.nskip           = 25;  % how many batches to skip for determining spike PCs
-ops.GPU                 = 1; % has to be 1, no CPU version yet, sorry
-% ops.Nfilt               = 1024; % max number of clusters
-ops.nfilt_factor        = 4; % max number of clusters per good channel (even temporary ones)
-ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
-ops.NT                  = 32*1024+ ops.ntbuff; % 64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory).
-ops.whiteningRange      = 32; % number of channels to use for whitening each channel
-ops.nSkipCov            = 25; % compute whitening matrix from every N-th batch
-ops.scaleproc           = 200;   % int16 scaling of whitened data
-ops.nPCs                = 3; % how many PCs to project the spikes into
-ops.useRAM              = 0; % not yet available
-ops.trange = [0 Inf]; % time range to sort
-ops.NchanTOT    = 384; % total number of channels in your recording
-% you need to change most of the paths in this block
-ops.fproc       = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
-ops.trange = [0 Inf]; % time range to sort
-ops.NchanTOT    = 385; % total number of channels in your recording
-
-%% this block runs all the steps of the algorithm
+%% 6) KS2 run
 fprintf('Looking for data inside %s \n', rootZ)
 
 % is there a channel map file in this folder?
@@ -94,7 +64,7 @@ fprintf('found %d good units \n', sum(rez.good>0))
 fprintf('Saving results to Phy  \n')
 rezToPhy(rez, rootZ);
 
-% get the commit hash on which this was run
+%% 7) WRAP-UP
 fid = fopen([rootZ filesep 'spike_sorting_ks2.log'], 'w+');
 for ff = fieldnames(ops)'
     val = ops.(ff{1});
@@ -107,11 +77,43 @@ for ff = fieldnames(ops)'
 end
 fclose(fid);
 
-% create the sync_merge flag
-fclose(fopen([rootZ filesep 'sync_merge_ephys.flag'], 'w+'));
-% create the compress ephys flag
-fclose(fopen([rootZ filesep 'compress_ephys.flag'], 'w+'));
+% cleanup temporary directory
+rmdir(scratch_dir, 's');
 
-
-%% Run the QC on KS2 output
-% [~, mess] = unix(['/home/olivier/Documents/PYTHON/iblscripts/deploy/serverpc/crontab/25_qc_spike_sorting.sh ' rootZ]);
+    function ops = ibl_ks2_params
+        ops.commitHash = strip(hash);
+        ops.chanMap = channel_map_file;
+        ops.fs = 30000;   % sample rate
+        ops.fshigh = 300;    % frequency for high pass filtering (150)
+        ops.minfr_goodchannels = 0;  % minimum firing rate on a "good" channel (0 to skip)
+        ops.Th = [10 4];   % threshold on projections (like in Kilosort1, can be different for last pass like [10 4])
+        ops.lam = 10;  % how important is the amplitude penalty (like in Kilosort1, 0 means not used, 10 is average, 50 is a lot)
+        ops.AUCsplit = 0.9; % splitting a cluster at the end requires at least this much isolation for each sub-cluster (max = 1)
+        ops.minFR = 1/50; % minimum spike rate (Hz), if a cluster falls below this for too long it gets removed
+        ops.momentum = [20 400]; % number of samples to average over (annealed from first to second value)
+        ops.sigmaMask = 30; % spatial constant in um for computing residual variance of spike
+        ops.ThPre = 8;  % threshold crossings for pre-clustering (in PCA projection space)
+        ops.CAR = true;  % Common Average Referencing (median)
+        % DANGER ZONE: changing these settings can lead to fatal errors
+        % options for determining PCs
+        ops.spkTh           = -6;      % spike threshold in standard deviations (-6)
+        ops.reorder         = 1;       % whether to reorder batches for drift correction.
+        ops.nskip           = 25;  % how many batches to skip for determining spike PCs
+        ops.GPU                 = 1; % has to be 1, no CPU version yet, sorry
+        % ops.Nfilt               = 1024; % max number of clusters
+        ops.nfilt_factor        = 4; % max number of clusters per good channel (even temporary ones)
+        ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
+        ops.NT                  = 32*1024+ ops.ntbuff; % 64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory).
+        ops.whiteningRange      = 32; % number of channels to use for whitening each channel
+        ops.nSkipCov            = 25; % compute whitening matrix from every N-th batch
+        ops.scaleproc           = 200;   % int16 scaling of whitened data
+        ops.nPCs                = 3; % how many PCs to project the spikes into
+        ops.useRAM              = 0; % not yet available
+        ops.trange = [0 Inf]; % time range to sort
+        ops.NchanTOT    = 384; % total number of channels in your recording
+        % you need to change most of the paths in this block
+        ops.fproc       = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
+        ops.trange = [0 Inf]; % time range to sort
+        ops.NchanTOT    = 385; % total number of channels in your recording
+    end
+end
