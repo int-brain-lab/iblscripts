62808c0ab97d26ec60689aea8c6af4c0435f9709
olivier <olivier.winter@hotmail.fr>
2020-09-23 15:04:36 +0100

    canary option on local servers

--- deploy/serverpc/crontab/update_ibllib.sh
+++ deploy/serverpc/crontab/update_ibllib.sh
@@ -7,10 +7,18 @@ git checkout -f master
 git reset --hard
 git pull
 # second step is to update ibllib
+# canary_branch.txt is an optional text file containing the ibllib branch to be installed
+FILE=/mnt/s0/Data/Subjects/.canary_branch
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    branch="$(<$FILE)"
+else
+    branch="master"
+fi
 source ~/Documents/PYTHON/envs/iblenv/bin/activate
 # pip install --upgrade ibllib
 # pip install -U mtscomp
 pip uninstall -y ibllib
 pip uninstall -y phylib
 pip install git+https://github.com/cortex-lab/phylib.git@ibl_tests
-pip install git+https://github.com/int-brain-lab/ibllib.git@master
+pip install git+https://github.com/int-brain-lab/ibllib.git@$branch
