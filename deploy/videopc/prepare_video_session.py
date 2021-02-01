1311a882a1a8e709e6252b76824b5fdaed731fe9
N <nbonacchi@gmail.com>
2021-02-01 19:26:37 +0000

    new workflows and changes to the preapare session

--- deploy/videopc/prepare_video_session.py
+++ deploy/videopc/prepare_video_session.py
@@ -37,29 +37,38 @@ def main(mouse: str, training_session: bool = False) -> None:
     print(f"Found {cams.NUM_CAMERAS} cameras. Trigger mode - ON")
     # Create filenames to call Bonsai
     filename = '_iblrig_{}Camera.raw.avi'
+    filenameframedata = '_iblrig_{}Camera.FrameData.csv'
     filenamets = '_iblrig_{}Camera.timestamps.ssv'
     filenamefc = '_iblrig_{}Camera.frame_counter.bin'
     filenameGPIO = '_iblrig_{}Camera.GPIO.bin'
-    # Open n start Bonsai view
-    body = "-p:FileNameBody=" + str(SESSION_FOLDER / filename.format('body'))
-    left = "-p:FileNameLeft=" + str(SESSION_FOLDER / filename.format('left'))
-    right = "-p:FileNameRight=" + str(SESSION_FOLDER / filename.format('right'))
-    bodyts = "-p:FileNameBodyTimestamps=" + str(SESSION_FOLDER / filenamets.format('body'))
-    leftts = "-p:FileNameLeftTimestamps=" + str(SESSION_FOLDER / filenamets.format('left'))
-    rightts = "-p:FileNameRightTimestamps=" + str(SESSION_FOLDER / filenamets.format('right'))
-    bodyfc = "-p:FileNameBodyFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('body'))
-    leftfc = "-p:FileNameLeftFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('left'))
-    rightfc = "-p:FileNameRightFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('right'))
-    bodyGPIO = "-p:FileNameBodyGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('body'))
-    leftGPIO = "-p:FileNameLeftGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('left'))
-    rightGPIO = "-p:FileNameRightGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('right'))
-
+    # Define parameters to call bonsai
     bodyidx = "-p:BodyCameraIndex=" + str(PARAMS['BODY_CAM_IDX'])
     leftidx = "-p:LeftCameraIndex=" + str(PARAMS['LEFT_CAM_IDX'])
     rightidx = "-p:RightCameraIndex=" + str(PARAMS['RIGHT_CAM_IDX'])
 
+    body = "-p:FileNameBody=" + str(SESSION_FOLDER / filename.format('body'))
+    left = "-p:FileNameLeft=" + str(SESSION_FOLDER / filename.format('left'))
+    right = "-p:FileNameRight=" + str(SESSION_FOLDER / filename.format('right'))
+
+    bodydata = "-p:FileNameBodyData=" + str(SESSION_FOLDER / filenameframedata.format('body'))
+    leftdata = "-p:FileNameLefDatat=" + str(SESSION_FOLDER / filenameframedata.format('left'))
+    rightdata = "-p:FileNameRightData=" + str(SESSION_FOLDER / filenameframedata.format('right'))
+
+    bodyts = "-p:FileNameBodyTimestamps=" + str(SESSION_FOLDER / filenamets.format('body'))
+    leftts = "-p:FileNameLeftTimestamps=" + str(SESSION_FOLDER / filenamets.format('left'))
+    rightts = "-p:FileNameRightTimestamps=" + str(SESSION_FOLDER / filenamets.format('right'))
+
+    bodyfc = "-p:FileNameBodyFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('body'))
+    leftfc = "-p:FileNameLeftFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('left'))
+    rightfc = "-p:FileNameRightFrameCounter=" + str(SESSION_FOLDER / filenamefc.format('right'))
+
+    bodyGPIO = "-p:FileNameBodyGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('body'))
+    leftGPIO = "-p:FileNameLeftGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('left'))
+    rightGPIO = "-p:FileNameRightGPIO=" + str(SESSION_FOLDER / filenameGPIO.format('right'))
+
     start = '--start'  # --start-no-debug
     noboot = '--no-boot'
+    noeditor = '--no-editor'
     # Open the streaming file and start
     subprocess.call([str(BONSAI), str(STREAM_FILE), start, noboot,
                      bodyidx, leftidx, rightidx])
@@ -67,6 +76,7 @@ def main(mouse: str, training_session: bool = False) -> None:
     subprocess.call([str(BONSAI), str(RECORD_FILE), noboot, body, left, right,
                      bodyidx, leftidx, rightidx, bodyts, leftts, rightts,
                      bodyfc, leftfc, rightfc, bodyGPIO, leftGPIO, rightGPIO])
+    subprocess.call(['python', '-V'])
     # Create a transfer_me.flag file
     open(SESSION_FOLDER.parent / 'transfer_me.flag', 'w')
     return
