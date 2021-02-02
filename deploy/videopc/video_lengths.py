c2f51298e58f8ae712848f56dc25e0c34e320abf
N <nbonacchi@gmail.com>
2021-02-02 19:58:13 +0000

    Added support for old data output or new *FrameData.cs file

--- deploy/videopc/video_lengths.py
+++ deploy/videopc/video_lengths.py
@@ -11,6 +11,19 @@ import sys
 from pathlib import Path
 import numpy as np
 import cv2
+import pandas as pd
+
+
+def load_CameraFrameData_file(session_path, camera: str) -> pd.DataFrame:
+    session_path = Path(session_path)
+    if session_path is None:
+        return
+    raw_path = Path(session_path).joinpath("raw_video_data")
+    # Check if frame data file exists
+    frame_data_file = raw_path.joinpath(f"_iblrig_{camera}Camera.FrameData.csv")
+    if frame_data_file.exists():
+        fdata = pd.read_csv(frame_data_file)
+        return fdata
 
 
 def load_embedded_frame_data(session_path, camera: str, raw=False):
@@ -20,19 +33,20 @@ def load_embedded_frame_data(session_path, camera: str, raw=False):
     :param raw: If True the raw data are returned without preprocessing (thresholding, etc.)
     :return: The frame counter, the pin state
     """
+    session_path = Path(session_path)
     if session_path is None:
         return None, None
-    raw_path = Path(session_path).joinpath('raw_video_data')
+    raw_path = Path(session_path).joinpath("raw_video_data")
     # Load frame count
-    count_file = raw_path / f'_iblrig_{camera}Camera.frame_counter.bin'
+    count_file = raw_path / f"_iblrig_{camera}Camera.frame_counter.bin"
     count = np.fromfile(count_file, dtype=np.float64).astype(int) if count_file.exists() else None
     if not (count is None or raw):
         count -= count[0]  # start from zero
     # Load pin state
-    pin_file = raw_path / f'_iblrig_{camera}Camera.GPIO.bin'
+    pin_file = raw_path / f"_iblrig_{camera}Camera.GPIO.bin"
     pin_state = np.fromfile(pin_file, dtype=np.float64).astype(int) if pin_file.exists() else None
     if not (pin_state is None or raw):
-        pin_state = pin_state > PIN_STATE_THRESHOLD
+        pin_state = pin_state > 10
     return count, pin_state
 
 
@@ -42,9 +56,8 @@ def get_video_length(video_path):
     :param video_path: A path to the video
     :return:
     """
-    is_url = isinstance(video_path, str) and video_path.startswith('http')
     cap = cv2.VideoCapture(str(video_path))
-    assert cap.isOpened(), f'Failed to open video file {video_path}'
+    assert cap.isOpened(), f"Failed to open video file {video_path}"
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     cap.release()
     return length
@@ -54,13 +67,42 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("I need a session_path as input...")
     session_path = Path(sys.argv[1])
-    video_lengths = [get_video_length(p) for p in session_path.rglob('*.avi')]
-    array_lengths = [(a.size, b.size) for a, b in [load_embedded_frame_data(session_path, cam, raw=True) for cam in ('left', 'right', 'body')]]
-    frame_counter_lengths = [x[0] for x in array_lengths]
-    GPIO_state_lengths = [x[1] for x in array_lengths]
-    print('\n',
-        sys.argv[1], '\n',
-        sorted(video_lengths), '<-- Video lengths', '\n',
-        sorted(frame_counter_lengths), '<-- Frame counter lengths', '\n',
-        sorted(GPIO_state_lengths), '<-- GPIO state lengths', '\n',
-    )
+    video_lengths = [get_video_length(p) for p in session_path.rglob("*.avi")]
+    data_frames = [
+        load_CameraFrameData_file(session_path, camera=c) for c in ("left", "right", "body")
+    ]
+    len_frames = [len(df) for df in data_frames if df is not None]
+    if not len_frames:
+        array_lengths = [
+            (a.size, b.size)
+            for a, b in [
+                load_embedded_frame_data(session_path, cam, raw=True)
+                for cam in ("left", "right", "body")
+            ]
+            if (a is not None) or (b is not None)
+        ]
+
+        array_len = []
+        for cam in ("left", "right", "body"):
+            a, b = load_embedded_frame_data(session_path, cam, raw=True)
+            if (a is not None) or (b is not None):
+                array_len.append((a.size, b.size))
+
+        frame_counter_lengths = [x[0] for x in array_lengths]
+        GPIO_state_lengths = [x[1] for x in array_lengths]
+        print(
+            "\n",
+            sys.argv[1], "\n",
+            sorted(video_lengths), "<-- Video lengths", "\n",
+            sorted(frame_counter_lengths), "<-- Frame counter lengths", "\n",
+            sorted(GPIO_state_lengths), "<-- GPIO state lengths", "\n",
+        )
+    else:
+        print(
+            "\n",
+            sys.argv[1], "\n",
+            sorted(video_lengths), "<-- Video lengths", "\n",
+            sorted(len_frames), "<-- Frame Data lengths", "\n",
+        )
+
+    # session_path = r"C:\iblrig_data\Subjects\_iblrig_test_mouse\2000-01-01\001"
