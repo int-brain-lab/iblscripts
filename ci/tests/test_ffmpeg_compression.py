96314b2aa9fa0cc40f3b97ce1fa5d0a91baeb0f3
Miles Wells <k1o0@3tk.co>
2021-03-07 11:30:59 +0200

    Mock QC

--- ci/tests/test_ffmpeg_compression.py
+++ ci/tests/test_ffmpeg_compression.py
@@ -4,7 +4,6 @@ import unittest.mock
 from pathlib import Path
 
 from ibllib.pipes import ephys_preprocessing, training_preprocessing
-from oneibl.one import ONE
 
 from ci.tests import base
 
@@ -50,33 +49,30 @@ class TestVideoTraining(base.IntegrationTest):
         self.TRAINING_INIT_FOLDER = self.data_path.joinpath('Subjects_init')
         assert self.TRAINING_INIT_FOLDER.exists()
 
+    @unittest.mock.patch('ibllib.pipes.training_preprocessing.CameraQC')
     @unittest.mock.patch('ibllib.io.extractors.camera.extract_all')
-    def test_compress_qc(self, mock_ext):
+    def test_compress(self, mock_ext, mock_qc):
         """
-        Here we're testing the video compression and QC only.  The timestamp extraction is
+        Here we're testing the video compression only.  The timestamp extraction and camera QC is
         stubbed and will not return any files.
         :param mock_ext: A stubbed camera extractor class.  Outputs no files.
+        :param mock_qc: A stubbed camera qc class.  Does nothing.
         :return:
         """
         vid_files = list(self.TRAINING_INIT_FOLDER.rglob('*.avi'))
         mock_ext.return_value = (None, [])  # Return empty file list upon timestamp extraction
-        one = ONE(base_url='https://test.alyx.internationalbrainlab.org',
-                  username='test_user',
-                  password='TapetesBloc18')
         for vid_file in vid_files:
             init_session_path = vid_file.parents[1]
             with tempfile.TemporaryDirectory() as tdir:
                 session_path = Path(tdir).joinpath(
                     init_session_path.relative_to(self.TRAINING_INIT_FOLDER))
                 shutil.copytree(init_session_path, session_path)
-                job = training_preprocessing.TrainingVideoCompress(session_path, one=one)
+                job = training_preprocessing.TrainingVideoCompress(session_path)
                 job.run()
                 self.assertIsNone(next(session_path.rglob('*.avi'), None))
                 self.assertEqual(next(session_path.rglob('*.mp4')), job.outputs[0])
-            # Test QC updated
-            eid = one.eid_from_path(init_session_path)
-            session_dict = one.alyx.rest('sessions', 'read', id=eid)
-            self.assertTrue('videoLeft' in session_dict['extended_qc'], 'qc field not updated')
+            mock_qc.assert_called_once()
+            mock_qc.reset_mock()
 
 
 if __name__ == "__main__":
