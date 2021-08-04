717e8815af724b27c3f4355f820d0bb1283c19ad
olivier <olivier.winter@hotmail.fr>
2021-08-04 15:51:46 +0200

    iblscripts is in Python root dir

--- deploy/serverpc/kilosort2/run_pykilosort_model.sh
+++ deploy/serverpc/kilosort2/run_pykilosort_model.sh
@@ -11,4 +11,4 @@ SCRATCH_DRIVE=/mnt/h0  # this is the path of the scratch SSD volume for intermed
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate pyks2
 export LD_LIBRARY_PATH=/usr/local/cuda-$CUDA_VERSION/lib64:/usr/local/cuda-$CUDA_VERSION/extras/CUPTI/lib64:/lib/nccl/cuda-$CUDA_VERSION:$LD_LIBRARY_PATH;
-python ~/Documents/PYTHON/00_IBL/iblscripts/deploy/serverpc/kilosort2/run_pykilosort.py $1 $SCRATCH_DRIVE
+python ~/Documents/PYTHON/iblscripts/deploy/serverpc/kilosort2/run_pykilosort.py $1 $SCRATCH_DRIVE
