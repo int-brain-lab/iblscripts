b7335d9948204c40aa36b272fa6b3f491fd29597
olivier <olivier.winter@hotmail.fr>
2021-10-18 09:49:44 +0100

    rename laser datasets to avoid underscores

--- ci/tests/test_personal_projects_extractions.py
+++ ci/tests/test_personal_projects_extractions.py
@@ -61,7 +61,7 @@ class TestEphysTaskExtraction(base.IntegrationTest):
 
     def test_ephys_biased_opto(self):
         """Guido's task"""
-        desired_output = list(EPHYS_TRIALS_SIGNATURE) + ['_ibl_trials.laser_probability.npy', '_ibl_trials.laser_stimulation.npy']
+        desired_output = list(EPHYS_TRIALS_SIGNATURE) + ['_ibl_trials.laserProbability.npy', '_ibl_trials.laserStimulation.npy']
         session_path = self.data_path.joinpath("personal_projects/ephys_biased_opto/ZFM-01802/2021-03-10/001")
         shutil.move(session_path.joinpath('alf'), session_path.joinpath('alf.bk'))
         _logger.info(f"{session_path}")
@@ -74,7 +74,7 @@ class TestEphysTaskExtraction(base.IntegrationTest):
 
 class TestTrainingTaskExtraction(base.IntegrationTest):
     """
-    The bpod jsonable can optionally contains 'laser_stimulation' and 'laser_probability' fields
+    The bpod jsonable can optionally contains 'laserStimulation' and 'laserProbability' fields
     Sometimes only the former. THe normal biased extractor detects them automatically
     """
 
@@ -82,7 +82,7 @@ class TestTrainingTaskExtraction(base.IntegrationTest):
         self.one_offline = ONE(mode='local')
 
     def test_biased_opto(self):
-        desired_output = list(TRAINING_TRIALS_SIGNATURE) + ['_ibl_trials.laser_stimulation.npy']
+        desired_output = list(TRAINING_TRIALS_SIGNATURE) + ['_ibl_trials.laserStimulation.npy']
         # this session has only laser stimulation labeled
         session_path = self.data_path.joinpath("personal_projects/biased_opto/ZFM-01804/2021-01-15/001")
         if session_path.joinpath('alf').exists():
@@ -92,12 +92,12 @@ class TestTrainingTaskExtraction(base.IntegrationTest):
         assert task.status == 0
         assert set([p.name for p in task.outputs]) == set(desired_output)
         trials = check_trials_and_clean_up(session_path)
-        assert (np.all(np.unique(trials.laser_stimulation) == np.array([0, 1])))
+        assert (np.all(np.unique(trials.laserStimulation) == np.array([0, 1])))
         assert set([p.name for p in task.outputs]) == set(desired_output)
 
         # this session has both laser probability and laser stimulation fields labeled
         desired_output = list(TRAINING_TRIALS_SIGNATURE) + \
-                         ['_ibl_trials.laser_probability.npy', '_ibl_trials.laser_stimulation.npy']
+                         ['_ibl_trials.laserProbability.npy', '_ibl_trials.laserStimulation.npy']
         session_path = self.data_path.joinpath("personal_projects/biased_opto/ZFM-01802/2021-02-08/001")
         if session_path.joinpath('alf').exists():
             shutil.move(session_path.joinpath('alf'), session_path.joinpath('alf.bk'))
@@ -106,8 +106,8 @@ class TestTrainingTaskExtraction(base.IntegrationTest):
         assert task.status == 0
         assert set([p.name for p in task.outputs]) == set(desired_output)
         trials = check_trials_and_clean_up(session_path)
-        assert(np.all(np.unique(trials.laser_stimulation) == np.array([0, 1])))
-        assert(np.all(np.logical_and(trials.laser_probability >= 0, trials.laser_probability <= 1)))
+        assert(np.all(np.unique(trials.laserStimulation) == np.array([0, 1])))
+        assert(np.all(np.logical_and(trials.laserProbability >= 0, trials.laserProbability <= 1)))
 
 
 def check_trials_and_clean_up(session_path):
