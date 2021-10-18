c70bd4832570ae2dba82f63bdb66354fd9aded5a
juhuntenburg <ju.huntenburg@gmail.com>
2021-10-18 18:47:43 +0100

    Add two new jobs, run_small and run_large

--- deploy/serverpc/crontab/00_ALL.sh
+++ deploy/serverpc/crontab/00_ALL.sh
@@ -7,8 +7,11 @@ source ~/Documents/PYTHON/envs/iblenv/bin/activate
 python jobs.py kill create
 python jobs.py create /mnt/s0/Data/Subjects &
 
-python jobs.py kill run
-python jobs.py run /mnt/s0/Data/Subjects &
+python jobs.py kill run_small
+python jobs.py run_small /mnt/s0/Data/Subjects &
+
+python jobs.py kill run_large
+python jobs.py run_large /mnt/s0/Data/Subjects &
 
 python jobs.py kill report
 python jobs.py report &
