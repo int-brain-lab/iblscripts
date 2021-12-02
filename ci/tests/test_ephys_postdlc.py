d7ff49837dbf6a3dc4d7a5583ffdec2701b96e03
juhuntenburg <ju.huntenburg@gmail.com>
2021-12-02 09:00:24 +0000

    PostDLC suppress plot and clean up

--- ci/tests/test_ephys_postdlc.py
+++ ci/tests/test_ephys_postdlc.py
@@ -8,37 +8,36 @@ from ibllib.pipes.ephys_preprocessing import EphysPostDLC
 
 
 class TestEphysPostDLC(base.IntegrationTest):
-
-	def setUp(self) -> None:
-		self.root_folder = self.data_path.joinpath('dlc', 'test_data')
-		if not self.root_folder.exists():
+	
+	@classmethod
+	def setUpClass(cls) -> None:
+		cls.root_folder = cls.default_data_root().joinpath('dlc', 'test_data')
+		if not cls.root_folder.exists():
 			return
 		# Run the task, without qc as we don't use a real session here and qc requires the database
-		self.task = EphysPostDLC(self.root_folder)
-		self.task.run(overwrite=True, run_qc=False)
-		# Load integration test target data, replace NaNs with 0 to enable comparison
-		self.target_data = dict()
-		self.target_data['licks'] = np.load(self.root_folder.joinpath('targets', 'licks.times.npy'))
-		for cam in ['left', 'right']:
-			features = pd.read_parquet(self.root_folder.joinpath('targets', f'_ibl_{cam}Camera.features.pqt')).fillna(0)
-			self.target_data[f'pupil_raw_{cam}'] = features['pupilDiameter_raw']
-			self.target_data[f'pupil_smooth_{cam}'] = features['pupilDiameter_smooth']
-
-	def tearDown(self) -> None:
-		for out in self.task.outputs:
+		cls.task = EphysPostDLC(cls.root_folder)
+		cls.task.run(overwrite=True, run_qc=False, plot_qc=False)
+	
+	@classmethod
+	def tearDownClass(cls) -> None:
+		for out in cls.task.outputs:
 			out.unlink()
 
 	def test_pupil_diameter(self):
+		# Load integration test target data, replace NaNs with 0 to enable comparison
 		for cam in ['left', 'right']:
+			target_features = pd.read_parquet(self.root_folder.joinpath('targets', f'_ibl_{cam}Camera.features.pqt')).fillna(0)
 			fname = [f for f in self.task.outputs if f'_ibl_{cam}Camera.features.pqt' in f.parts][0]
 			features = pd.read_parquet(fname).fillna(0)
-			self.assertTrue(all(features['pupilDiameter_raw'] == self.target_data[f'pupil_raw_{cam}']))
-			self.assertTrue(all(features['pupilDiameter_smooth'] == self.target_data[f'pupil_smooth_{cam}']))
+			self.assertTrue(all(features['pupilDiameter_raw'] == target_features['pupilDiameter_raw']))
+			self.assertTrue(all(features['pupilDiameter_smooth'] == target_features['pupilDiameter_smooth']))
 
 	def test_licks_times(self):
+		# Load integration test target data
+		target_licks = np.load(self.root_folder.joinpath('targets', 'licks.times.npy'))
 		fname = [f for f in self.task.outputs if 'licks.times.npy' in f.parts][0]
 		licks = np.load(fname)
-		self.assertTrue(all(licks == self.target_data['licks']))
+		self.assertTrue(all(licks == target_licks))
 
 
 if __name__ == "__main__":
