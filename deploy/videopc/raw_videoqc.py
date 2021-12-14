641dc66df5ba0a28e470d7bdbb3618e71489a836
Miles Wells <k1o0@3tk.co>
2021-12-14 16:00:48 +0200

    Bug fixes for video QC script

--- deploy/videopc/raw_videoqc.py
+++ deploy/videopc/raw_videoqc.py
@@ -4,22 +4,30 @@
 # @Author: Niccolo' Bonacchi (@nbonacchi)
 # @Date: Thursday, October 28th 2021, 3:37:12 pm
 import argparse
-import sys
 from pathlib import Path
 
 from ibllib.qc.camera import CameraQC
-from one.api import ONE
+from ibllib.io.raw_data_loaders import load_camera_ssv_times
+from one.api import One
 
 
-def main(session_path, display=False):
-    qc = CameraQC(session_path, 'left', one=ONE(mode='local', silent=True), stream=False)
-    qc.video_path = session_path.joinpath('raw_video_data', f'_iblrig_{qc.label}Camera.raw.avi')
+def main(session_path, display=False, seesion_type=None, label='left'):
+    qc = CameraQC(session_path, label, one=One(mode='local'), stream=False)
+    qc.video_path = next(session_path.joinpath('raw_video_data').glob(f'*{qc.label}Camera.raw*'))
+    qc._type = seesion_type
+    if not qc.type:
+        n_videos = len(list(qc.video_path.parent.glob('*Camera.raw*')))
+        qc._type = 'ephys' if n_videos > 1 else 'training'
+    # NB: In later QC the timestamps will be the extracted camera.times.  Used in framerate check.
+    qc.data['timestamps'] = load_camera_ssv_times(session_path, camera=qc.label)
     qc.load_video_data()
+
     # Run frame checks
     bright_outcome = qc.check_brightness(display=display)
     pos_outcome = qc.check_position(display=display)
     focus_outcome = qc.check_focus(display=display)
     print(f"Brightness: {bright_outcome}\nPosition: {pos_outcome}\nFocus: {focus_outcome}")
+
     # Run meta data checks
     fh_outcome = qc.check_file_headers()
     fr_outcome = qc.check_framerate()
@@ -33,6 +41,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Run video QC on raw files')
     parser.add_argument('session_path', help='Local session path')
+    parser.add_argument('-c', '--camera', default='left',
+                        help='Camera label, i.e. left, right or body')
+    parser.add_argument('-t', '--type', default=None, required=False,
+                        help='Session type, e.g. ephys or training')
     parser.add_argument(
         '-d', '--display',
         action="store_true",
@@ -41,5 +53,5 @@ if __name__ == '__main__':
         help='Whether to display plots'
     )
     args = parser.parse_args()
-    main(Path(args.session_path), display=args.display)
+    main(Path(args.session_path), display=args.display, seesion_type=args.type)
 
