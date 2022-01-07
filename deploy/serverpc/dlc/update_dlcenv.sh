0792de8bd40994da0c6bf8c68a0f09ce0241d32b
juhuntenburg <ju.huntenburg@gmail.com>
2022-01-07 12:03:17 +0100

    Update dlc env

--- deploy/serverpc/dlc/update_dlcenv.sh
+++ deploy/serverpc/dlc/update_dlcenv.sh
@@ -1,5 +1,13 @@
+# update the iblvideo package
 cd ~/Documents/PYTHON/iblvideo
 git fetch --all
 git checkout -f master
 git reset --hard
 git pull
+
+# update the environment
+source ~/Documents/PYTHON/envs/dlcenv/bin/activate
+pip uninstall -y ibllib
+pip install git+https://github.com/int-brain-lab/ibllib.git@master
+pip install -U tensorflow
+pip install -U deeplabcut
