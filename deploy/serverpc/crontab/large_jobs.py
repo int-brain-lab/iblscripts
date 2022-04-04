4b286100b6312aa76381a388ebd0afe83bf768b5
juhuntenburg <ju.huntenburg@gmail.com>
2022-04-04 11:44:21 +0100

    Logging

--- deploy/serverpc/crontab/large_jobs.py
+++ deploy/serverpc/crontab/large_jobs.py
@@ -1,31 +1,30 @@
 import traceback
 import time
-
+import logging
 from pathlib import Path
 
 from one.api import ONE
 from ibllib.pipes.local_server import task_queue
 from ibllib.pipes.tasks import run_alyx_task
 
+_logger = logging.getLogger(__name__)
 subjects_path = Path('/mnt/s0/Data/Subjects/')
+sleep_time = 3600
 
 try:
     one = ONE(cache_rest=None)
     waiting_tasks = task_queue(mode='large', lab=None, one=one)
 
     if len(waiting_tasks) == 0:
-        # TODO: proper logging
-        print("No large tasks in the queue")
-        # Query again only in 60 min if queue is empty
-        time.sleep(3600)
+        _logger.info(f"No large tasks in the queue, retrying in {int(sleep_time / 60)} min")
+        # Query again only in 60min if queue is empty
+        time.sleep(sleep_time)
     else:
         tdict = waiting_tasks[0]
-        # TODO: proper logging
-        print(f"Running task {tdict['name']} for session {tdict['session']}")
+        _logger.info(f"Running task {tdict['name']} for session {tdict['session']}")
         ses = one.alyx.rest('sessions', 'list', django=f"pk,{tdict['session']}")[0]
         session_path = Path(subjects_path).joinpath(
             Path(ses['subject'], ses['start_time'][:10], str(ses['number']).zfill(3)))
         run_alyx_task(tdict=tdict, session_path=session_path, one=one)
 except BaseException:
-    # TODO: proper logging
-    print(traceback.format_exc())
+    _logger.error(f"Error running large task queue \n {traceback.format_exc()}")
