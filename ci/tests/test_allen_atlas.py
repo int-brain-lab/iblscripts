30f4339d236ca4e0f2c272a76a36c7698853212e
olivier <olivier.winter@hotmail.fr>
2022-05-02 15:37:28 +0100

    Swanson atlas tests

--- ci/tests/test_allen_atlas.py
+++ ci/tests/test_allen_atlas.py
@@ -1,5 +1,7 @@
 import numpy as np
-from ibllib.atlas import AllenAtlas, FlatMap
+import matplotlib.pyplot as plt
+from ibllib.atlas import AllenAtlas, FlatMap, BrainRegions
+from ibllib.atlas.flatmaps import plot_swanson
 
 
 def test_simple():
@@ -33,3 +35,33 @@ def test_flatmaps():
 
     fm = FlatMap(flatmap='pyramid')
     fm.plot_flatmap(volume='image')
+
+
+def test_swanson():
+    br = BrainRegions()
+
+    # prepare array of acronyms
+    acronyms = ['ACAd1', 'ACAv1', 'AId1', 'AIp1', 'AIv1', 'AUDd1', 'AUDp1', 'AUDpo1', 'AUDv1',
+                'SSp-m1', 'SSp-n1', 'SSp-tr1', 'SSp-ul1', 'SSp-un1', 'SSs1',
+                'VISC1', 'VISa1', 'VISal1', 'VISam1', 'VISl1', 'VISli1', 'VISp1', 'VISp2/3', 'VISpl1', 'VISpm1',
+                'SSp-n2/3', 'SSp-tr2/3', 'SSp-ul2/3', 'SSp-un2/3', 'SSs2/3',
+                'VISC2/3', 'VISa2/3', 'VISal2/3', 'VISam2/3', 'VISl2/3', 'VISli2/3', 'VISp2/3', 'VISpl1', 'VISpl2/3']
+
+    # assign data to each acronym
+    values = np.arange(len(acronyms))
+
+    regions_rl = np.r_[br.acronym2id(acronyms), -br.acronym2id(acronyms)]
+    values_rl = np.random.randn(regions_rl.size)
+
+    fig, ax = plt.subplots()
+    plot_swanson(hemisphere='both', br=br)
+    fig, ax = plt.subplots()
+    plot_swanson(br=br)
+    fig, ax = plt.subplots()
+    plot_swanson(acronyms, values, cmap='Blues', br=br)
+    fig, ax = plt.subplots()
+    plot_swanson(acronyms, values, cmap='Blues', hemisphere='both')
+    fig, ax = plt.subplots()
+    plot_swanson(regions_rl, values_rl, hemisphere='both', cmap='magma', br=br)
+
+    plt.close('all')
