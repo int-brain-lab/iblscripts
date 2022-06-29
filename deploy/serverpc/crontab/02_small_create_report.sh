3c7ceb5e9af10911da5f6eb383d78e2813373d68
juhuntenburg <ju.huntenburg@gmail.com>
2022-06-29 19:07:30 +0200

    WIP Starting to add maintenance and report

---
+++ deploy/serverpc/crontab/02_small_create_report.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Go to crontab dir
+cd ~/Documents/PYTHON/iblscripts/deploy/serverpc/crontab
+# Source iblenv
+source ~/Documents/PYTHON/envs/iblenv/bin/activate
+
+env_update_in=0 # this is to make sure the environments get updated upon service restart
+env_last_update=$SECONDS  # $SECONDS indicates seconds since shell was opened, which we use a zero point
+
+report_create_elapsed=7200  # this is to make sure report and create run upon service restart
+report_create_last=$SECONDS  # $SECONDS indicates seconds since shell was opened, which we use a zero point
+
+while true; do
+  # Every night at midnight, run environment update and maintenance script
+  if  (( SECONDS - env_last_update >= env_update_in )); then
+    printf "\n$(date)\n" ;
+    printf "Checking iblenv for updates\n" ;
+    printf "Logging to /var/log/ibl/update_iblenv.log\n" ;
+    ./update_iblenv.sh >> /var/log/ibl/update_iblenv.log 2>&1 ;
+    printf "\n$(date)\n" ;
+    printf "Running maintenance script\n" ;
+    printf "Logging to /var/log/ibl/maintenance.log\n" ;
+    python maintenance.py >> /var/log/ibl/maintenance.log 2>&1 ;
+    # Reset time to next update to time until next midnight (in seconds) and restart counting elapsed seconds
+    env_update_in=$(expr `date -d "tomorrow 0" +%s` - `date -d "now" +%s`) ;
+    env_last_update=$SECONDS ;
+  fi
+  # If more than two hours have passed, report health and check for create jobs
+  if  (( SECONDS - report_create_last >= 7200 )); then
+    printf "\n$(date)\n" ;
+    printf "Running health report and creating jobs\n" ;
+    printf "Logging to /var/log/ibl/report_create.log\n" ;
+    python report_create.py >>/ var/log/ibl/report_create.log 2>&1 ;
+    report_create_last=$SECONDS  # reset the timer
+  fi
+  # Always: query for waiting jobs and run first job in the queue
+  printf "\n$(date)\n" ;
+  printf "Running next set of small jobs from the queue\n" ;
+  printf "Logging to /var/log/ibl/small_jobs.log\n" ;
+  python small_jobs.py >> /var/log/ibl/small_jobs.log 2>&1 ;
+  # Repeat
+done
+
+# add create continuously
+# add report_health every 2 hours
+# add maintenance at midnight
--- deploy/serverpc/crontab/small_jobs.py
+++ deploy/serverpc/crontab/small_jobs.py
@@ -18,21 +18,25 @@ try:
 
     if len(waiting_tasks) == 0:
         _logger.info(f"No small tasks in the queue, retrying in {int(sleep_time / 60)} min")
-        # Query again only in 60min if queue is empty
+        # Sleep for 60min if queue is empty
         time.sleep(sleep_time)
     else:
         # In the case of small tasks we run a set of them at a time before re-querying
         # Often they are from the same session, so we cache the session path between tasks
         last_session = None
-        for i in range(count):
-            tdict = waiting_tasks[i]
+        c = 0
+        for tdict in waiting_tasks:
+            if c >= count:
+                break
             _logger.info(f"Running task {tdict['name']} for session {tdict['session']}")
             if last_session != tdict['session']:
                 ses = one.alyx.rest('sessions', 'list', django=f"pk,{tdict['session']}")[0]
                 session_path = Path(subjects_path).joinpath(
                     Path(ses['subject'], ses['start_time'][:10], str(ses['number']).zfill(3)))
                 last_session = tdict['session']
-            run_alyx_task(tdict=tdict, session_path=session_path, one=one)
+            task, dsets = run_alyx_task(tdict=tdict, session_path=session_path, one=one)
+            if dsets:
+                c += 1  # i.e. only tasks that output datasets are counted towards count
 except BaseException:
     _logger.error(f"Error running small task queue \n {traceback.format_exc()}")
     time.sleep(int(sleep_time / 2))
