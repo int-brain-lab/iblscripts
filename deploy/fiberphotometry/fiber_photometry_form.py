3fa7f5109c0c92027c6807bc15fdbc2102a3fa71
Michele Fabbri <michele.fabbri@internationalbrainlab.org>
2022-07-19 03:03:06 +0100

    additional widget values being stored in qsettings, reasonable default values set, app layout modified

--- deploy/fiberphotometry/fiber_photometry_form.py
+++ deploy/fiberphotometry/fiber_photometry_form.py
@@ -2,14 +2,13 @@
 Application to perform Fiber Photometry related tasks
 
 TODO:
-- use QSettings to store additional widget data
 - parse up csv file when adding item to queue
     - create additional pd file for each 'item'
 - call ibllib when initiating the transfer
 
 QtSettings values:
-    path_last_loaded_csv: str - path to the parent dir of the last loaded csv
-    path_server_sessions: str - destination path for local lab server, i.e  \\mainenlab_server\Subjects
+    last_loaded_csv_path: str - path to the parent dir of the last loaded csv
+    server_path: str - destination path for local lab server, i.e  \\mainenlab_server\Subjects
     subjects: list[str] = field(default_factory=list) - list of subjects should carry over between sessions
 """
 import argparse
@@ -39,6 +38,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent=parent)
         self.setupUi(self)
         self.model = None
+        self.items_to_transfer = []
 
         # init QSettings
         self.settings = QtCore.QSettings("int-brain-lab", "fiber_photometry_form")
@@ -54,30 +54,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.statusBar().addWidget(self.status_bar_message)
 
         # Populate widgets
-        self.date_edit.setDate(QtCore.QDate(date.today().year, date.today().month, date.today().day))
-        self.populate_widgets_with_dummy_data()  # using dummy data for now
+        self.populate_widgets()
 
-    def populate_widgets_with_dummy_data(self):
-        # subject_combo_box
-        subject_dummy_data = ["mouse1", "mouse2", "mouse3"]
-        self.settings.setValue("subjects", subject_dummy_data)
-        for value in self.settings.value("subjects"):
-            self.subject_combo_box.addItem(value)
+    def populate_widgets(self):
+        """
+        Pulls from stored QSettings values where appropriate, and sets reasonable defaults to others
+        """
+        # date
+        self.date_edit.setDate(QtCore.QDate(date.today().year, date.today().month, date.today().day))
+        # subject
+        if self.settings.value("subjects") is not None:
+            for value in self.settings.value("subjects"):  # TODO: reverse loop to get last entry as default?
+                self.subject_combo_box.addItem(value)
+        else:
+            self.settings.setValue("subjects", ["mouse1"])  # dummy data
+        # server_path
+        if self.settings.value("path_server_session") is not None:
+            self.server_path.setText(self.settings.value("server_path"))
+        else:
+            self.server_path.setText("\\\\path_to_server\\Subjects")  # dummy data
 
         # session_number
         self.session_number.setText("001")
 
-        # server_path
-        self.server_path.addItem("\\\\mainen_lab_server\\Subjects")
-
     def transfer_items_to_server(self):
+        # if subject value in items_to_transfer list dict is not in self.settings.value("subject"), then add it to the settings
         print("transfer button press, call ibllib rsync transfer")
+        transfer_success = True
+
+        if transfer_success:
+            # Add subject to QSettings if it is not already present
+            subject_list = self.settings.value("subjects")
+            if self.subject_combo_box.currentText() not in subject_list:
+                subject_list.append(self.subject_combo_box.currentText())
+                self.settings.setValue("subjects", subject_list)
+
+            # Set server path to QSettings
+            self.settings.setValue("server_path", self.server_path.text())
 
     def add_item_to_queue(self):
         """
-        Verifies that all entered values are present. A cleaner implementation is desirable.
+        Verifies that all entered values are present. A cleaner implementation is desirable for this function.
         """
-        # Pull data
+        # Pull data for ROIs
         checked_rois = []
         checked_rois.append("Region0R") if self.cb_region0r.isChecked() else None
         checked_rois.append("Region1G") if self.cb_region1g.isChecked() else None
@@ -89,30 +108,48 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         checked_rois.append("Region7R") if self.cb_region7r.isChecked() else None
         checked_rois.append("Region8G") if self.cb_region8g.isChecked() else None
 
+        # Pull data for patch cord
         checked_patches = []
         checked_patches.append("Patch1") if self.cb_patch1.isChecked() else None
         checked_patches.append("Patch2") if self.cb_patch2.isChecked() else None
         checked_patches.append("Patch3") if self.cb_patch3.isChecked() else None
 
-        self.items_to_transfer = {
+        # Build out items to transfer list
+        self.items_to_transfer.append({
             "subject": self.subject_combo_box.currentText(),
             "date": self.date_edit.text(),
             "session_number": self.session_number.text(),
             "rois": checked_rois,
             "patches": checked_patches,
-            "server_path": self.server_path.currentText()
-        }
+            "server_path": self.server_path.text()
+        })
 
-        stringified_items_to_transfer = self.items_to_transfer["subject"] + " - " + self.items_to_transfer["date"] + " - " + \
-                                        self.items_to_transfer["session_number"] + " - "
-        for values in self.items_to_transfer["rois"]:
-            stringified_items_to_transfer += values + " "
-        for values in self.items_to_transfer["patches"]:
-            stringified_items_to_transfer += values + " "
-        stringified_items_to_transfer += "-> " + self.items_to_transfer["server_path"]
+        stringified_item_to_transfer = self.stringify_items_to_transfer(self.items_to_transfer[-1])
 
         # Display data that will be added to the queue
-        self.item_list_queue.addItem(stringified_items_to_transfer)
+        self.item_list_queue.addItem(stringified_item_to_transfer)
+
+    def stringify_items_to_transfer(self, item_to_transfer: dict) -> str:
+        """
+        Method to build out a string representation for the item that we are preparing to transfer
+
+        Parameters
+        ----------
+        item_to_transfer: dict
+            The following keys are exected: subject, date, session_number, rois, patches, server_path
+        Returns
+        -------
+        str
+            representation for the item we are preparing to transfer
+        """
+        return_string = item_to_transfer["subject"] + " - " + item_to_transfer["date"] + " - " + \
+                        item_to_transfer["session_number"] + " - "
+        for values in item_to_transfer["rois"]:
+            return_string += values + " "
+        for values in item_to_transfer["patches"]:
+            return_string += values + " "
+        return_string += "-> " + item_to_transfer["server_path"]
+        return return_string
 
     def load_csv(self, file=None):
         """
@@ -128,11 +165,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         if file is None or file is False:
             file, _ = QtWidgets.QFileDialog.getOpenFileName(
                 parent=self, caption="Select Raw Fiber Photometry Recording",
-                directory=self.settings.value("path_last_loaded_csv"), filter="*.csv")
+                directory=self.settings.value("last_loaded_csv_path"), filter="*.csv")
         if file == "":
             return
         file = Path(file)
-        self.settings.setValue("path_last_loaded_csv", str(file.parent))
+        self.settings.setValue("last_loaded_csv_path", str(file.parent))
         self.model = Model(pd.read_csv(file))
 
         # Change status bar text
@@ -212,6 +249,6 @@ if __name__ == "__main__":
         test_controller(fiber_form, csv_file)
     else:
         # Disable actionable widgets until CSV is loaded
-        fiber_form.enable_actionable_widgets(False)
+        # fiber_form.enable_actionable_widgets(False)
         fiber_form.show()
         sys.exit(app.exec_())
