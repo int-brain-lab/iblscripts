68621abc068ecfe131925c9ca57ec4edbb9d0fd9
juhuntenburg <ju.huntenburg@gmail.com>
2022-10-14 12:06:09 +0100

    Fix flaking errors

--- deploy/serverpc/crontab/maintenance_jobs.py
+++ deploy/serverpc/crontab/maintenance_jobs.py
@@ -345,7 +345,7 @@ def dynamic_pipeline_transition_photometry():
 if __name__ == "__main__":
     correct_flags_biased_in_ephys_rig()
     # correct_ephys_manual_video_copies()
-    remove_iti_duration()
+    # remove_iti_duration()
     # spike_amplitude_patching()
     # upload_ks2_output()
     correct_passive_in_wrong_folder()
