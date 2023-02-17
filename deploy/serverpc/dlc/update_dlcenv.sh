03a32bdf6716de72f02cd196647bccb3c9ae28d9
juhuntenburg <ju.huntenburg@gmail.com>
2023-02-17 18:14:37 +0200

    no more updating for tensorflow and dlc for now

--- deploy/serverpc/dlc/update_dlcenv.sh
+++ deploy/serverpc/dlc/update_dlcenv.sh
@@ -25,45 +25,46 @@ if test "$update" ; then
   pip install --upgrade pip
 fi
 
-# These libraries need to be installed in order, so if one is updated, the ones after need to be updated too
-update=$(echo $outdated | grep -o "tensorflow " | cut -d = -f 1)
-# Note that the space after tensorflow is crucial as the tensorflow-estimator otherwise keeps tensorflow being updated
+# Note: no updating for tensorflow and deeplabcut for now because this has been messing with our env
+## These libraries need to be installed in order, so if one is updated, the ones after need to be updated too
+#update=$(echo $outdated | grep -o "tensorflow " | cut -d = -f 1)
+## Note that the space after tensorflow is crucial as the tensorflow-estimator otherwise keeps tensorflow being updated
+#if test "$update" ; then
+#  echo "Updating tensorflow, deeplabcut and ibllib" ;
+#  pip uninstall -y ONE-api ibllib deeplabcut tensorflow ;
+#  pip install tensorflow ;
+#  pip install deeplabcut ;
+#  pip install ibllib ;
+#  pip install ONE-api ;
+#else
+#  echo "tensorflow is up-to-date" ;
+#  update=$(echo $outdated | grep -o "deeplabcut" | cut -d = -f 1)
+#  if test "$update" ; then
+#    echo "Updating deeplabcut and ibllib" ;
+#    pip uninstall -y ONE-api ibllib deeplabcut ;
+#    pip install deeplabcut ;
+#    pip install ibllib ;
+#    pip install ONE-api ;
+#  else
+#    echo "deeplabcut is up-to-date" ;
+update=$(echo $outdated | grep -o "ibllib" | cut -d = -f 1)
 if test "$update" ; then
-  echo "Updating tensorflow, deeplabcut and ibllib" ;
-  pip uninstall -y ONE-api ibllib deeplabcut tensorflow ;
-  pip install tensorflow ;
-  pip install deeplabcut ;
+  echo "Updating ibllib" ;
+  pip uninstall -y ONE-api ibllib ;
   pip install ibllib ;
   pip install ONE-api ;
 else
-  echo "tensorflow is up-to-date" ;
-  update=$(echo $outdated | grep -o "deeplabcut" | cut -d = -f 1)
+  echo "ibllib is up-to-date" ;
+  update=$(echo $outdated | grep -o "ONE-api" | cut -d = -f 1)
   if test "$update" ; then
-    echo "Updating deeplabcut and ibllib" ;
-    pip uninstall -y ONE-api ibllib deeplabcut ;
-    pip install deeplabcut ;
-    pip install ibllib ;
+    echo "Updating ONE-api" ;
+    pip uninstall -y ONE-api ;
     pip install ONE-api ;
   else
-    echo "deeplabcut is up-to-date" ;
-    update=$(echo $outdated | grep -o "ibllib" | cut -d = -f 1)
-    if test "$update" ; then
-      echo "Updating ibllib" ;
-      pip uninstall -y ONE-api ibllib ;
-      pip install ibllib ;
-      pip install ONE-api ;
-    else
-      echo "ibllib is up-to-date" ;
-      update=$(echo $outdated | grep -o "ONE-api" | cut -d = -f 1)
-      if test "$update" ; then
-        echo "Updating ONE-api" ;
-        pip uninstall -y ONE-api ;
-        pip install ONE-api ;
-      else
-        echo "ONE-api is up-to-date" ;
-      fi
-    fi
+    echo "ONE-api is up-to-date" ;
   fi
 fi
+#  fi
+#fi
 
 deactivate
