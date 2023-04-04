d6b8966c20acb82acc40f2c2a03211001099728b
juhuntenburg <ju.huntenburg@gmail.com>
2023-04-04 17:12:32 +0200

    force un and reinstall ibllib in dlcenv

--- deploy/serverpc/dlc/update_dlcenv.sh
+++ deploy/serverpc/dlc/update_dlcenv.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Make sure local iblvideo repository is up to date
-cd ~/Documents/PYTHON/iblvideo
-git checkout -f master -q
-git reset --hard -q
-git fetch
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "@{u}")
-if [ $LOCAL != $REMOTE ]; then
-  echo "Updating iblvideo"
-  git pull
-else
-  echo "iblvideo is up-to-date"
-fi
-
 # Check if any pip libraries are out of date if yes, update
 source ~/Documents/PYTHON/envs/dlcenv/bin/activate
 echo "Updating ibllib mesoscope"
-pip install --upgrade git+https://github.com/int-brain-lab/ibllib.git@mesoscope --upgrade-strategy eager ;
+pip uninstall -y ibllib
+pip install git+https://github.com/int-brain-lab/ibllib.git@mesoscope
