2e00e0d55a0f6fb7454355bccc8d993ade951cc7
Miles Wells <k1o0@5tk.co>
2023-05-16 18:34:26 +0300

    various tests

--- ci/tests/tasks/test_mesoscope_tasks.py
+++ ci/tests/tasks/test_mesoscope_tasks.py
@@ -1,17 +1,23 @@
 import logging
 import shutil
+import tempfile
 import unittest.mock
+from pathlib import Path
 from unittest.mock import MagicMock, ANY
 
 import numpy as np
+import pandas as pd
 from numpy.testing import assert_array_almost_equal
 from iblutil.util import Bunch
 import one.alf.io as alfio
+from one.alf.files import get_session_path
 from one.api import ONE
 
-from ibllib.pipes.mesoscope_tasks import MesoscopeSync, MesoscopeFOV
+from ibllib.pipes.mesoscope_tasks import \
+    MesoscopeSync, MesoscopeFOV, MesoscopeRegisterSnapshots, MesoscopePreprocess, MesoscopeCompress
 from ibllib.io.extractors.ephys_fpga import get_wheel_positions
 from ibllib.io.extractors import mesoscope
+from ibllib.io.raw_daq_loaders import load_timeline_sync_and_chmap
 
 from ci.tests import base
 
@@ -32,7 +38,9 @@ class TestTimelineTrials(base.IntegrationTest):
         # task = ChoiceWorldTrialsTimeline(self.session_path, sync_collection='raw_mesoscope_data', sync_namespace='timeline')
         # status = task.run()
         # assert status == 0
-        session_path = self.default_data_root().joinpath('mesoscope', 'SP037', '2023-02-20', '001')
+        # session_path = self.default_data_root().joinpath('mesoscope', 'SP037', '2023-02-20', '001')
+        session_path = self.default_data_root().joinpath('mesoscope', 'SP037', '2023-03-09', '001')
+        session_path = self.default_data_root().parent.joinpath('resources', 'SP035', '2023-03-03', '001')
         from ibllib.pipes.dynamic_pipeline import make_pipeline
         pipe = make_pipeline(session_path, one=self.one)
         # TODO Rename raw_timeline_data -> raw_sync_data
@@ -45,7 +53,9 @@ class TestTimelineTrials(base.IntegrationTest):
         # plot_timeline(timeline, channels, raw=False)
         #bpod_t[1987]
         # Out[20]: 2764.5935 (< 0.0005)
+        pipe.tasks['ChoiceWorldTrialsTimeline_00'].one = pipe.one
         status = pipe.tasks['ChoiceWorldTrialsTimeline_00'].run()
+        # status = pipe.tasks['PassiveTaskTimeline_01'].run()
         self.assertFalse(status)
 
     def test_get_wheel_positions(self):
@@ -107,9 +117,9 @@ class TestTimelineTrials(base.IntegrationTest):
         self.assertCountEqual({-1, 1}, np.unique(y))
 
     def test_timeline2sync(self):
-        """Test for ibllib.io.extractors.mesoscope._timeline2sync."""
-        timeline = alfio.load_object(self.session_path_2 / 'raw_sync_data', 'DAQData')
-        sync, chmap = mesoscope.timeline2sync(timeline)
+        """Test for ibllib.io.raw_daq_loaders.load_timeline_sync_and_chmap."""
+        sync, chmap = mesoscope.load_timeline_sync_and_chmap(
+            self.session_path_2 / 'raw_sync_data', save=False)
         self.assertIsInstance(sync, dict)
         self.assertCountEqual(('times', 'channels', 'polarities'), sync.keys())
         expected = {
@@ -151,7 +161,7 @@ class TestMesoscopeSync(base.IntegrationTest):
         # Check output
         nROIs = 9
         alf_path = self.session_path.joinpath('alf')
-        ROI_folders = list(alf_path.rglob('ROI*'))
+        ROI_folders = list(filter(Path.is_dir, alf_path.rglob('FOV*')))
         self.assertEqual(nROIs, len(ROI_folders))
         ROI_times = list(alf_path.rglob('mpci.times.npy'))
         self.assertEqual(nROIs, len(ROI_times))
@@ -161,3 +171,106 @@ class TestMesoscopeSync(base.IntegrationTest):
         self.assertEqual(nROIs, len(ROI_shifts))
         expected = [0., 4.157940e-05, 8.315880e-05, 1.247382e-04, 1.663176e-04]
         np.testing.assert_array_almost_equal(np.load(ROI_shifts[0])[:5], expected)
+
+    def test_mesoscope_sync_multiple(self):
+        session_path = self.default_data_root().joinpath('mesoscope', 'SP037', '2023-03-09', '001')
+        task = MesoscopeSync(session_path, device_collection='raw_imaging_data*', one=self.one)
+        status = task.run()
+        assert status == 0
+
+        # Check output
+        nROIs = 6
+        alf_path = session_path.joinpath('alf')
+        ROI_folders = list(filter(Path.is_dir, alf_path.rglob('FOV*')))
+        self.assertEqual(nROIs, len(ROI_folders))
+        ROI_times = list(alf_path.rglob('mpci.times.npy'))
+        self.assertEqual(nROIs, len(ROI_times))
+        expected = [1.0075, 1.1535, 1.2995, 1.446, 1.592]
+        np.testing.assert_array_almost_equal(np.load(ROI_times[0])[:5], expected)
+        ROI_shifts = list(alf_path.rglob('mpciStack.timeshift.npy'))
+        self.assertEqual(nROIs, len(ROI_shifts))
+        expected = [0., 4.157550e-05, 8.315100e-05, 1.247265e-04, 1.663020e-04]
+        np.testing.assert_array_almost_equal(np.load(ROI_shifts[0])[:5], expected)
+
+
+class TestMesoscopeRegisterSnapshots(base.IntegrationTest):
+    session_path = None
+
+    def setUp(self) -> None:
+        self.one = ONE(**base.TEST_DB)
+        self.session_path = self.default_data_root().joinpath('mesoscope', 'test', '2023-02-17', '002')
+        self.session_path = Path(r'F:\FlatIron\resources\SP035\2023-03-01\001')
+
+    def test_register_snapshots(self):
+        task = MesoscopeRegisterSnapshots(self.session_path, one=self.one)
+        eid, *_ = self.one.search()
+        with unittest.mock.patch.object(self.one, 'path2eid', return_value=eid):
+            status = task.run()
+        self.assertEqual(0, status)
+        notes = self.one.alyx.rest('notes', 'list', session=eid)  # filter doesn't work
+        self.assertEqual(4, len([n for n in notes if n['image'] and n['object_id'] == eid]))
+
+    def test_get_signature(self):
+        for i in range(2):
+            p = self.session_path.joinpath(
+                f'raw_imaging_data_{i:02}', 'reference', '2023-03-01_1_SP035_2P_00001_00001.tif')
+            p.parent.mkdir(parents=True, exist_ok=True)
+            p.touch(), self.addCleanup(p.unlink)
+
+        task = MesoscopeRegisterSnapshots(self.session_path, one=self.one)
+        task.get_signatures()
+
+
+class TestMesoscopePreprocess(base.IntegrationTest):
+    """Test for MesoscopePreprocess task."""
+    def setUp(self) -> None:
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.addCleanup(self.tempdir.cleanup)
+        self.session_path = self.default_data_root().joinpath('mesoscope', 'SP037', '2023-03-23', '002')
+        self.alf_path = self.session_path.joinpath('alf', 'suite2p', 'plane2')
+        self.rename_dict = {
+            'F.npy': 'mpci.ROIActivityF.npy',
+            'spks.npy': 'mpci.ROIActivityDeconvolved.npy',
+            'Fneu.npy': 'mpci.ROINeuropilActivityF.npy'}
+        # Copy files to temp dir
+        self.suite2pdir = Path(self.tempdir.name).joinpath(*self.alf_path.parts[-6:])
+        shutil.copytree(self.alf_path, self.suite2pdir)
+        self.one = ONE(**base.TEST_DB)
+
+    def test_rename_outputs(self):
+        """Test MesoscopePreprocess._rename_outputs method."""
+        session_path = get_session_path(self.suite2pdir)
+        task = MesoscopePreprocess(session_path, one=self.one)
+        files = task._rename_outputs(self.suite2pdir.parent, None, None)
+        self.assertTrue(all(map(Path.exists, files)))
+        self.assertFalse(self.suite2pdir.exists())
+        self.assertTrue((compressed := files[0].with_name('_suite2p_ROIData.raw.zip')).exists())
+        self.assertIn(compressed, files)
+        # Check files saved transposed
+        for old, new in self.rename_dict.items():
+            expected = np.load(self.alf_path / old).T
+            np.testing.assert_array_equal(expected, np.load(files[0].with_name(new)))
+        # Check frame QC not saved
+        self.assertFalse(any('mpciFrameQC' in f.name for f in files))
+
+    def test_rename_with_qc(self):
+        """Test MesoscopePreprocess._rename_outputs method with frame QC input."""
+        session_path = get_session_path(self.suite2pdir)
+        task = MesoscopePreprocess(session_path, one=self.one)
+        # Check frameQC is saved
+        frameQC_names = pd.DataFrame([(0, 'ok'), (1, 'foo')], columns=['qc_values', 'qc_labels'])
+        files = task._rename_outputs(self.suite2pdir.parent, frameQC_names, np.zeros(15))
+        self.assertIn(files[0].with_name('mpciFrameQC.names.tsv'), files)
+        self.assertIn(files[0].with_name('mpci.mpciFrameQC.npy'), files)
+
+
+class TestMesoscopeCompress(base.IntegrationTest):
+    """Test for MesoscopeCompress task."""
+    def setUp(self) -> None:
+        self.session_path = self.default_data_root() / 'mesoscope' / 'test' / '2023-03-03' / '002'
+        self.session_path = self.default_data_root() / 'mesoscope' / 'test' / '2023-02-17' / '002'  # Small
+        self.one = ONE(**base.TEST_DB)
+
+    def test_compress(self):
+        task = MesoscopeCompress(self.session_path, one=self.one)
+        files = task.run()
