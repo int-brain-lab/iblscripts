77f1412b6d91df9deaceef149d202a63699866f5
juhuntenburg <ju.huntenburg@gmail.com>
2023-06-21 13:10:18 +0200

    restore update dlc env

--- deploy/serverpc/dlc/update_dlcenv.sh
+++ deploy/serverpc/dlc/update_dlcenv.sh
@@ -1,7 +1,70 @@
 #!/bin/bash
 
+# Make sure local iblvideo repository is up to date
+cd ~/Documents/PYTHON/iblvideo
+git checkout -f master -q
+git reset --hard -q
+git fetch
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "@{u}")
+if [ $LOCAL != $REMOTE ]; then
+  echo "Updating iblvideo"
+  git pull
+else 
+  echo "iblvideo is up-to-date"
+fi
+
 # Check if any pip libraries are out of date if yes, update
 source ~/Documents/PYTHON/envs/dlcenv/bin/activate
-echo "Updating ibllib mesoscope"
-pip uninstall -y ibllib
-pip install git+https://github.com/int-brain-lab/ibllib.git@mesoscope
+outdated=$(pip list --outdated | awk 'NR>2 {print $1}')
+
+# Check if pip needs update
+update=$(echo $outdated | grep -o "pip" | cut -d = -f 1)
+if test "$update" ; then
+  echo "Updating pip" ;
+  pip install --upgrade pip
+fi
+
+# Note: no updating for tensorflow and deeplabcut for now because this has been messing with our env
+## These libraries need to be installed in order, so if one is updated, the ones after need to be updated too
+#update=$(echo $outdated | grep -o "tensorflow " | cut -d = -f 1)
+## Note that the space after tensorflow is crucial as the tensorflow-estimator otherwise keeps tensorflow being updated
+#if test "$update" ; then
+#  echo "Updating tensorflow, deeplabcut and ibllib" ;
+#  pip uninstall -y ONE-api ibllib deeplabcut tensorflow ;
+#  pip install tensorflow ;
+#  pip install deeplabcut ;
+#  pip install ibllib ;
+#  pip install ONE-api ;
+#else
+#  echo "tensorflow is up-to-date" ;
+#  update=$(echo $outdated | grep -o "deeplabcut" | cut -d = -f 1)
+#  if test "$update" ; then
+#    echo "Updating deeplabcut and ibllib" ;
+#    pip uninstall -y ONE-api ibllib deeplabcut ;
+#    pip install deeplabcut ;
+#    pip install ibllib ;
+#    pip install ONE-api ;
+#  else
+#    echo "deeplabcut is up-to-date" ;
+update=$(echo $outdated | grep -o "ibllib" | cut -d = -f 1)
+if test "$update" ; then
+  echo "Updating ibllib" ;
+  pip uninstall -y ONE-api ibllib ;
+  pip install ibllib ;
+  pip install ONE-api ;
+else
+  echo "ibllib is up-to-date" ;
+  update=$(echo $outdated | grep -o "ONE-api" | cut -d = -f 1)
+  if test "$update" ; then
+    echo "Updating ONE-api" ;
+    pip uninstall -y ONE-api ;
+    pip install ONE-api ;
+  else
+    echo "ONE-api is up-to-date" ;
+  fi
+fi
+#  fi
+#fi
+
+deactivate
