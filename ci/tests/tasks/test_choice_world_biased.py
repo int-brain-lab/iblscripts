3e2e925b1ad7ee2cde927b023c7bb9190bb87852
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2023-07-14 10:45:21 +0100

    pass in one=(mode = local) so qc not updated

--- ci/tests/tasks/test_choice_world_biased.py
+++ ci/tests/tasks/test_choice_world_biased.py
@@ -1,6 +1,8 @@
 import logging
 import shutil
 import ibllib.pipes.behavior_tasks as btasks
+from one.api import ONE
+from one.registration import RegistrationClient
 
 from ci.tests import base
 
@@ -10,6 +12,7 @@ _logger = logging.getLogger('ibllib')
 class BiasedTemplate(base.IntegrationTest):
 
     def setUp(self) -> None:
+        self.one = ONE(**base.TEST_DB, mode='local')
         self.raw_session_path = next(self.default_data_root().joinpath(
             'tasks', 'choice_world_biased').rglob('raw_behavior_data')).parent
         self.session_path, self.extraction_path = base.make_sym_links(self.raw_session_path)
@@ -21,7 +24,7 @@ class BiasedTemplate(base.IntegrationTest):
 class TestBiasedTrialsBpod(BiasedTemplate):
 
     def test_task(self):
-        wf = btasks.ChoiceWorldTrialsBpod(self.session_path, collection='raw_behavior_data')
+        wf = btasks.ChoiceWorldTrialsBpod(self.session_path, one=self.one, collection='raw_behavior_data')
         status = wf.run(update=False)
         assert status == 0
         wf.assert_expected_outputs()
--- ci/tests/tasks/test_choice_world_habituation.py
+++ ci/tests/tasks/test_choice_world_habituation.py
@@ -1,6 +1,7 @@
 import logging
 import shutil
 import ibllib.pipes.behavior_tasks as btasks
+from one.api import ONE
 
 from ci.tests import base
 
@@ -10,6 +11,7 @@ _logger = logging.getLogger('ibllib')
 class HabituationTemplate(base.IntegrationTest):
 
     def setUp(self) -> None:
+        self.one = ONE(**base.TEST_DB, mode='local')
         self.raw_session_path = next(self.default_data_root().joinpath(
             'tasks', 'choice_world_habituation').rglob('raw_behavior_data')).parent
         self.session_path, self.extraction_path = base.make_sym_links(self.raw_session_path)
@@ -21,7 +23,7 @@ class HabituationTemplate(base.IntegrationTest):
 class TestHabituationRegisterRaw(HabituationTemplate):
 
     def test_task(self):
-        wf = btasks.HabituationRegisterRaw(self.session_path, collection='raw_behavior_data')
+        wf = btasks.HabituationRegisterRaw(self.session_path, one=self.one, collection='raw_behavior_data')
         status = wf.run()
         assert status == 0
         wf.assert_expected_outputs()
