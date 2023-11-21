7392c5d3284f288ef5b092a560b56fbdc8bdcdc4
Miles Wells <k1o0@5tk.co>
2023-11-21 16:30:50 +0200

    Clearer output with subtest; better handling of fixture backup; assert validation data present

--- ci/tests/test_ephys_trials.py
+++ ci/tests/test_ephys_trials.py
@@ -1,4 +1,5 @@
 import shutil
+import warnings
 
 import numpy as np
 import numpy.testing
@@ -14,7 +15,6 @@ from ci.tests import base
 BPOD_FILES = [
     '_ibl_trials.table.pqt',
     '_ibl_trials.goCueTrigger_times.npy',
-    '_ibl_trials.intervals_bpod.npy',
     '_ibl_trials.quiescencePeriod.npy'
 ]
 
@@ -25,6 +25,7 @@ ALIGN_BPOD_FPGA_FILES = [
 
 
 class TestEphysTaskExtraction(base.IntegrationTest):
+    """Test the FpgaTrials extractor."""
 
     def setUp(self) -> None:
         self.root_folder = self.data_path.joinpath('ephys')
@@ -48,15 +49,38 @@ class TestEphysTaskExtraction(base.IntegrationTest):
             with self.subTest(msg=session_path):
                 self._task_extraction_assertions(session_path)
 
+    @staticmethod
+    def _restore_alf(session_path):
+        """Restore backup alf folder."""
+        alf_path = session_path.joinpath('alf')
+        bk_path = alf_path.parent / 'alf.bk'
+        if alf_path.exists() and bk_path.exists():
+            shutil.rmtree(alf_path, ignore_errors=True)
+            shutil.move(str(bk_path), str(alf_path))
+
     def _task_extraction_assertions(self, session_path):
+        """Compare task extraction with expected ALF trials output."""
         alf_path = session_path.joinpath('alf')
         bk_path = alf_path.parent / 'alf.bk'
         if alf_path.exists():
             # Back-up alf files and restore on teardown
-            shutil.move(alf_path, bk_path)
-            self.addCleanup(shutil.move, str(bk_path), alf_path)
-        # this gets the full output
-        fpga_trials, _ = ephys_fpga.extract_all(session_path, save=True)
+            if bk_path.exists():  # if last cleanup failed
+                warnings.warn(f'{bk_path} already exists; removing alf path')
+                # assume backup is correct validation data and delete the alf folder
+                shutil.rmtree(alf_path, ignore_errors=True)
+            else:
+                shutil.move(alf_path, bk_path)
+            self.addCleanup(self._restore_alf, session_path)
+        elif not bk_path.exists():
+            raise ValueError(f'alf folder missing for session {session_path}')
+
+        from ibllib.pipes.behavior_tasks import ChoiceWorldTrialsNidq
+        task = ChoiceWorldTrialsNidq(session_path,
+                                     one=ONE(mode='local'), collection='raw_behavior_data',
+                                     sync_collection='raw_ephys_data')
+        fpga_trials, _ = task._extract_behaviour(save=True)
+        tqc_ephys = task._run_qc(fpga_trials.copy(), update=False, plot_qc=False)
+
         # check that the output is complete
         for f in BPOD_FILES:
             with self.subTest(file=f):
@@ -70,7 +94,8 @@ class TestEphysTaskExtraction(base.IntegrationTest):
             alf_trials_old = alfio.load_object(alf_path.parent / 'alf.bk', 'trials')
             for k, v in alf_trials.items():
                 if k in alf_trials_old:  # added quiescencePeriod from the old dataset
-                    numpy.testing.assert_array_almost_equal(v, alf_trials_old[k])
+                    with self.subTest(attribute=k):
+                        numpy.testing.assert_array_almost_equal(v, alf_trials_old[k])
         # go deeper and check the internal fpga trials structure consistency
         fpga_trials = {k: v for k, v in fpga_trials.items() if 'wheel' not in k}
         # check dimensions
@@ -87,32 +112,30 @@ class TestEphysTaskExtraction(base.IntegrationTest):
                                               np.isnan(fpga_trials['errorCue_times'][:-1]))))
 
         # do the task qc
-        # tqc_ephys.extractor.settings['PYBPOD_PROTOCOL']
-        ex = TaskQCExtractor(session_path, lazy=True, one=None, bpod_only=False)
-        ex.data = fpga_trials  # FIXME This line is pointless
-        ex.extract_data()
-
-        # '/mnt/s0/Data/IntegrationTests/ephys/ephys_choice_world_task/CSP004/2019-11-27/001'
-        tqc_ephys = TaskQC(session_path, one=ONE(mode='local'))
-        tqc_ephys.extractor = ex
         _, res_ephys = tqc_ephys.run(bpod_only=False, download_data=False)
 
         tqc_bpod = TaskQC(session_path, one=ONE(mode='local'))
+        tqc_bpod.extractor = TaskQCExtractor(session_path, lazy=True, one=None, bpod_only=True)
+        tqc_bpod.extractor.settings = task.extractor.settings
+        tqc_bpod.extractor.data = tqc_bpod.extractor.rename_data(task.extractor.bpod_trials.copy())
+        # tqc_bpod.extractor.frame_ttls = task.extractor.bpod_extractor.frame2ttl  # used in iblapps QC viewer
+        # tqc_bpod.extractor.audio_ttls = task.extractor.bpod_extractor.audio  # used in iblapps QC viewer
+        from ibllib.io.raw_data_loaders import load_bpod_fronts  # FIXME Remove these two lines and uncomment the above
+        tqc_bpod.extractor.frame_ttls, tqc_bpod.extractor.audio_ttls = (
+            load_bpod_fronts(session_path, data=task.extractor.bpod_extractor.bpod_trials, task_collection='raw_behavior_data'))
+
         _, res_bpod = tqc_bpod.run(bpod_only=True, download_data=False)
 
         # for a swift comparison using variable explorer
         # import pandas as pd
         # df = pd.DataFrame([[res_bpod[k], res_ephys[k]] for k in res_ephys], index=res_ephys.keys())
 
-        ok = True
         for k in res_ephys:
             if k == '_task_response_feedback_delays':
                 continue
-            if np.abs(res_bpod[k] - res_ephys[k]) > .2:
-                ok = False
-                print(f'{k} bpod: {res_bpod[k]}, ephys: {res_ephys[k]}')
-        assert ok
-        shutil.rmtree(alf_path, ignore_errors=True)
+            with self.subTest(check=k):
+                self.assertFalse(
+                    np.abs(res_bpod[k] - res_ephys[k]) > .2, f'{k} bpod: {res_bpod[k]}, ephys: {res_ephys[k]}')
 
 
 class TestEphysTrialsFPGA(base.IntegrationTest):
