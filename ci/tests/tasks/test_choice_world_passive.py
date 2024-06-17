43b717bbc7ac5dc322580d04c7249b85f9b8891a
Mayo Faulkner <mayo.faulkner@ucl.ac.uk>
2024-06-17 17:15:28 +0100

    test skipping of task replay extraction

--- ci/tests/tasks/test_choice_world_passive.py
+++ ci/tests/tasks/test_choice_world_passive.py
@@ -1,5 +1,9 @@
 import logging
 import shutil
+import unittest.mock
+
+import pandas as pd
+import numpy as np
 
 from one.api import ONE
 from ibllib.pipes.behavior_tasks import PassiveRegisterRaw, PassiveTaskNidq
@@ -40,7 +44,26 @@ class TestPassiveTrials(base.IntegrationTest):
         assert status == 0
         task.assert_expected_outputs()
 
-        # TODO test the validity of the outputs
+        assert len(task.outputs) == 4
+
+        passive_intervals = pd.read_csv(next(o for o in task.outputs
+                                             if '_ibl_passivePeriods.intervalsTable.csv' in o.name))
+        assert not np.all(np.isnan(passive_intervals.taskReplay.values))
+
+    @unittest.mock.patch("ibllib.io.extractors.ephys_passive.skip_task_replay", return_value=True)
+    def test_passive_extract_no_task_replay(self):
+        task = PassiveTaskNidq(self.session_path, collection='raw_passive_data', sync_collection='raw_ephys_data',
+                               sync_namespace='spikeglx', one=self.one)
+        status = task.run()
+
+        assert status == 0
+        task.assert_expected_outputs()
+
+        assert len(task.outputs) == 2
+
+        passive_intervals = pd.read_csv(next(o for o in task.outputs
+                                             if '_ibl_passivePeriods.intervalsTable.csv' in o.name))
+        assert np.all(np.isnan(passive_intervals.taskReplay.values))
 
     def tearDown(self) -> None:
         shutil.rmtree(self.alf_path)
