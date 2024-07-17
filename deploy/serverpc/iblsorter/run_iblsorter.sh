98afd480418da7fb4a66348f326382ca89080f7a
olivier <olivier.winter@hotmail.fr>
2024-07-17 08:31:47 +0100

    fix ignored files previously commited

--- deploy/serverpc/iblsorter/run_iblsorter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRATCH_DRIVE=/home/olivier/scratch  # this is the path of the scratch SSD volume for intermediate KS2 results swapping
-# --------------- DO NOT EDIT BELOW
-
-source ~/Documents/PYTHON/SPIKE_SORTING/ibl-sorter/.venv/bin/activate
-which python  # checks the version for easier debugging
-python ~/Documents/PYTHON/iblscripts/deploy/serverpc/kilosort2/run_pykilosort.py $1 $2
