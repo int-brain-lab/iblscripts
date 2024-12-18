fcdfdb2ede05d3c10817088673455dfeb3b28fcb
themattinthehatt <themattinthehatt@gmail.com>
2024-12-18 13:54:55 -0500

    dlc->pose

--- deploy/serverpc/litpose/run_motion.py
+++ deploy/serverpc/litpose/run_motion.py
@@ -1,14 +1,17 @@
 import argparse
 from pathlib import Path
+
 from iblvideo.motion_energy import motion_energy
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run motion energy for mp4 file and dlc outputs')
+
+    parser = argparse.ArgumentParser(description='Run motion energy for mp4 file and pose outputs')
     parser.add_argument('file_mp4', help='mp4 file to compute motion energy from')
-    parser.add_argument('dlc_result', help='dlc pqt file to compute motion energy from')
+    parser.add_argument('pose_result', help='pose pqt file to compute motion energy from')
     args = parser.parse_args()
 
     file_mp4 = Path(args.file_mp4)
-    dlc_result = Path(args.dlc_result)
+    pose_result = Path(args.pose_result)
 
-    me_result, me_roi = motion_energy(file_mp4, dlc_result)
+    me_result, me_roi = motion_energy(file_mp4, pose_result)
