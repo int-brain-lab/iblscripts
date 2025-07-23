e8925f6f2cf3ae614e89b2180428d6fe0df83dc4
themattinthehatt <themattinthehatt@gmail.com>
2025-07-23 16:11:19 -0400

    bug fix

--- deploy/serverpc/litaction/run_litaction.py
+++ deploy/serverpc/litaction/run_litaction.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Run lightning action for pose and wheel data')
     parser.add_argument('pose_file', help='pose file to run LA on')
-    parser.add_argument('pose_timestamp_file', help='timestamps associated with pose file')
+    parser.add_argument('pose_timestamps_file', help='timestamps associated with pose file')
     parser.add_argument('wheel_file', help='wheel file')
     parser.add_argument('wheel_timestamps_file', help='timestamps associated with wheel file')
     parser.add_argument('overwrite', help='whether to force overwrite existing intermediate outputs')
@@ -19,9 +19,9 @@ if __name__ == '__main__':
 
     la_result = lightning_action(
         pose_file=Path(args.pose_file),
-        pose_timestamp_file=Path(args.pose_timestamp_file),
+        pose_timestamp_file=Path(args.pose_timestamps_file),
         wheel_file=Path(args.wheel_file),
-        wheel_timestamp_file=Path(args.wheel_timestamp_file),
+        wheel_timestamp_file=Path(args.wheel_timestamps_file),
         ckpts_path=path_models,
         force=args.overwrite,
     )
