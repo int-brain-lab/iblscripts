f594e27e928dbc4f2afa666b3e20a71a388f873d
Miles Wells <k1o0@5tk.co>
2025-08-29 15:31:13 +0300

    Default to newCI branch

--- ci/00_prep_env.sh
+++ ci/00_prep_env.sh
@@ -32,7 +32,7 @@ if [[ "$branch" =~ ^(remotes\/origin\/)?master$ ]] || \
    [[ "$branch" =~ ^remotes\/origin\/HEAD$ ]] || \
    ! git rev-parse -q --verify --end-of-options $branch; then
         echo Checking out develop branch of iblscripts
-        git checkout develop
+        git checkout newCI
 else
         echo Checking out $branch of iblscripts
         git checkout $branch
